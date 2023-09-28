91163e8b93ee2186cbb56760193910b8483a6c50
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2023-09-28 11:06:26 +0330

    Update Helper.php

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -44,4 +44,57 @@ class Helper
 
         return $value;
     }
+
+    public function sendSMS() {
+        $username = "arsha-74390";
+        $password = "XuKAZDHX0i2USZKA";
+        $domain = "magfa";
+
+        //soap
+        $url = "https://sms.magfa.com/api/soap/sms/v2/server?wsdl";
+
+        //soap options
+        $options = [
+            'login'          => "$username/$domain", 'password' => $password, // -Credientials
+            'cache_wsdl'     => WSDL_CACHE_NONE, // -No WSDL Cache
+            'compression'    => (SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP | 5), // -Compression *
+            'trace'          => false, // -Optional (debug)
+            'stream_context' => stream_context_create(
+                [
+                    'ssl' => [
+                        'verify_peer'       => false,
+                        'verify_peer_name'  => false,
+                        'allow_self_signed' => true
+                    ]
+                ]
+            )
+        ];
+
+        // * Accept response compression and compress requests using gzip with compression level 5
+
+        // soap client
+
+        $client = new SoapClient($url, $options);
+
+        $mobile = $_POST['mobile'];
+        $message = $_POST['message'];
+
+        // send
+        $response = $result['send'] = $client->send(
+            [$message], // messages
+            ["+98300074390"], // short numbers can be 1 or same count as recipients (mobiles)
+            [$mobile], // recipients
+            [], // client-side unique IDs.
+            [], // Encodings are optional, The system will guess it, itself ;)
+            [], // UDHs, Please read Magfa UDH Documnet
+            [] // Message priorities (unused).
+        );
+
+        //$status = $response->status;
+        $id = $response->id;
+        //$parts = $response->parts;
+        //$tariff = $response->tariff;
+        echo $id;
+    }
+
 }
