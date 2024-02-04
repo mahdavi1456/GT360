5bfeedd54869add15fa3533a7a4523d992b7c841
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-04 12:41:11 +0330

    Update Primex Theme

--- resources/views/front/theme/primex/index.blade.php
+++ resources/views/front/theme/primex/index.blade.php
@@ -4,19 +4,19 @@
     <title>Home</title>
     <meta charset="utf-8">
     <meta name="format-detection" content="telephone=no"/>
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="css/grid.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/camera.css"/>
-    <link rel="stylesheet" href="css/owl.carousel.css"/>
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-migrate-1.2.1.js"></script>
-    <script src="js/jquery.equalheights.js"></script>
+    <link rel="icon" href="{{ asset('front-theme-asset/primex') }}/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('front-theme-asset/primex') }}/css/grid.css">
+    <link rel="stylesheet" href="{{ asset('front-theme-asset/primex') }}/css/style.css">
+    <link rel="stylesheet" href="{{ asset('front-theme-asset/primex') }}/css/camera.css"/>
+    <link rel="stylesheet" href="{{ asset('front-theme-asset/primex') }}/css/owl.carousel.css"/>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/jquery.js"></script>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/jquery-migrate-1.2.1.js"></script>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/jquery.equalheights.js"></script>
     <!--[if (gt IE 9)|!(IE)]><!-->
-    <script src="js/jquery.mobile.customized.min.js"></script>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/jquery.mobile.customized.min.js"></script>
     <!--<![endif]-->
-    <script src="js/camera.js"></script>
-    <script src="js/owl.carousel.js"></script>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/camera.js"></script>
+    <script src="{{ asset('front-theme-asset/primex') }}/js/owl.carousel.js"></script>
     <!--[if lt IE 9]>
     <div style=' clear: both; text-align:center; position: relative;'>
         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
@@ -42,7 +42,7 @@
                     <div class="brand put-left">
                         <h1>
                             <a href="index.html">
-                                <img src="images/logo.png" alt="Logo"/>
+                                <img src="{{ asset('front-theme-asset/primex') }}/images/logo.png" alt="Logo"/>
                             </a>
                         </h1>
                     </div>
@@ -82,19 +82,19 @@
 <section id="content"><div class="ic">More Website Templates @ TemplateMonster.com - September08, 2014!</div>
 <div class="camera-wrapper">
     <div id="camera" class="camera-wrap">
-        <div data-src="images/index_slide01.jpg">
+        <div data-src="{{ asset('front-theme-asset/primex') }}/images/index_slide01.jpg">
             <div class="fadeIn camera_caption">
                 <h2 class="text_1 color_1">Solutions that you need!</h2>
                 <a class="btn_1" href="#">More info</a>
             </div>
         </div>
-        <div data-src="images/index_slide02.jpg">
+        <div data-src="{{ asset('front-theme-asset/primex') }}/images/index_slide02.jpg">
             <div class="fadeIn camera_caption">
                 <h2 class="text_1 color_1">Lorem ipsum dolor sit!</h2>
                 <a class="btn_1" href="#">More info</a>
             </div>
         </div>
-        <div data-src="images/index_slide03.jpg">
+        <div data-src="{{ asset('front-theme-asset/primex') }}/images/index_slide03.jpg">
             <div class="fadeIn camera_caption">
                 <h2 class="text_1 color_1">Lorem ipsum dolor sit amet, consectetur!</h2>
                 <a class="btn_1" href="#">More info</a>
@@ -220,7 +220,7 @@
                         <div class="clearfix"></div>
                     </div>
                 </div>
-           
+
         </div>
     </div>
 </div>
@@ -299,6 +299,6 @@
         </div>
     </div>
 </footer>
-<script src="js/script.js"></script>
+<script src="{{ asset('front-theme-asset/primex') }}/js/script.js"></script>
 </body>
 </html>
