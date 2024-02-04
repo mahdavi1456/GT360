28a89ceb349e6c354c586c0a81ccffffba110a76
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-04 14:15:34 +0330

    Update index.blade.php

--- resources/views/front/theme/scrolled/index.blade.php
+++ resources/views/front/theme/scrolled/index.blade.php
@@ -98,19 +98,19 @@
 		<h2>Portfolio</h2>
 		<ul class="port-style">
 			<div class="diamondswrap" id="port-items">
-				<div class="item" style="background-image: url(images/portfolio/1.jpg);"><span class="item-hover"><h4>Project 1</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/2.jpg);"><span class="item-hover"><h4>Project 2</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/3.jpg);"><span class="item-hover"><h4>Project 3</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/4.jpg);"><span class="item-hover"><h4>Project 4</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/5.jpg);"><span class="item-hover"><h4>Project 5</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/6.jpg);"><span class="item-hover"><h4>Project 6</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/7.jpg);"><span class="item-hover"><h4>Project 7</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/8.jpg);"><span class="item-hover"><h4>Project 8</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/9.jpg);"><span class="item-hover"><h4>Project 9</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/1.jpg);"><span class="item-hover"><h4>Project 10</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/2.jpg);"><span class="item-hover"><h4>Project 11</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/3.jpg);"><span class="item-hover"><h4>Project 12</h4><a href="#">Take a look</a></span></div>
-				<div class="item" style="background-image: url(images/portfolio/4.jpg);"><span class="item-hover"><h4>Project 13</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/1.jpg);"><span class="item-hover"><h4>Project 1</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/2.jpg);"><span class="item-hover"><h4>Project 2</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/3.jpg);"><span class="item-hover"><h4>Project 3</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/4.jpg);"><span class="item-hover"><h4>Project 4</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/5.jpg);"><span class="item-hover"><h4>Project 5</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/6.jpg);"><span class="item-hover"><h4>Project 6</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/7.jpg);"><span class="item-hover"><h4>Project 7</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/8.jpg);"><span class="item-hover"><h4>Project 8</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/9.jpg);"><span class="item-hover"><h4>Project 9</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/1.jpg);"><span class="item-hover"><h4>Project 10</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/2.jpg);"><span class="item-hover"><h4>Project 11</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/3.jpg);"><span class="item-hover"><h4>Project 12</h4><a href="#">Take a look</a></span></div>
+				<div class="item" style="background-image: url({{ asset('front-theme-asset/scrolled') }}/images/portfolio/4.jpg);"><span class="item-hover"><h4>Project 13</h4><a href="#">Take a look</a></span></div>
 			</div>
 		</ul>
 	</section>
