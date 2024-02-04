73290c3b8ecd943575e6ffe40a0d3802cfeda2de
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-04 08:05:17 +0330

    Work on Gym Theme

--- resources/views/front/theme/gym/index.blade.php
+++ resources/views/front/theme/gym/index.blade.php
@@ -16,19 +16,19 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />-->
-<script src="js/jquery.min.js"></script>
+<script src="{{ asset('front-theme-asset/gym') }}/js/jquery.min.js"></script>
 <script type="text/javascript">
 		jQuery(document).ready(function($) {
-			$(".scroll").click(function(event){		
+			$(".scroll").click(function(event){
 				event.preventDefault();
 				$('html,body').animate({scrollTop:$(this.hash).offset().top},1200);
 			});
 		});
 	</script>
 <!-- grid-slider -->
-<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-<script type="text/javascript" src="js/jquery.contentcarousel.js"></script>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="{{ asset('front-theme-asset/gym') }}/js/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="{{ asset('front-theme-asset/gym') }}/js/jquery.contentcarousel.js"></script>
+<script type="text/javascript" src="{{ asset('front-theme-asset/gym') }}/js/jquery.easing.1.3.js"></script>
 <!-- //grid-slider -->
 </head>
 <body>
@@ -55,15 +55,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="header-bottom_left">
 				<i class="phone"> </i><span>1-200-346-2986</span>
 			</div>
-			<div class="social">	
-			   <ul>	
+			<div class="social">
+			   <ul>
 				  <li class="facebook"><a href="#"><span> </span></a></li>
 				  <li class="twitter"><a href="#"><span> </span></a></li>
-				  <li class="pinterest"><a href="#"><span> </span></a></li>	
+				  <li class="pinterest"><a href="#"><span> </span></a></li>
 				  <li class="google"><a href="#"><span> </span></a></li>
 				  <li class="tumblr"><a href="#"><span> </span></a></li>
-				  <li class="instagram"><a href="#"><span> </span></a></li>	
-				  <li class="rss"><a href="#"><span> </span></a></li>							
+				  <li class="instagram"><a href="#"><span> </span></a></li>
+				  <li class="rss"><a href="#"><span> </span></a></li>
 			   </ul>
 		   </div>
 		   <div class="clear"></div>
@@ -74,7 +74,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <div class="menu" id="menu">
 	  <div class="container">
 		 <div class="logo">
-			<a href="index.html"><img src="images/logo.png" alt=""/></a>
+			<a href="index.html"><img src="{{ asset('front-theme-asset/gym') }}/images/logo.png" alt=""/></a>
 		 </div>
 		 <div class="h_menu4"><!-- start h_menu4 -->
 		   <a class="toggleMenu" href="#">Menu</a>
@@ -83,13 +83,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			   <li><a href="about.html">About</a></li>
 			   <li><a href="trainers.html">Trainers</a></li>
 			   <li><a href="classes.html">Classes</a>
-			   	
+
 			   </li>
-			   <li><a href="blog.html">Blog</a></li>
-			   <li><a href="pricing.html">Pricing</a></li>
-			   <li><a href="contact.html">Contact</a></li>
+			   <li><a href="{{ asset('front-theme-asset/gym') }}/blog.html">Blog</a></li>
+			   <li><a href="{{ asset('front-theme-asset/gym') }}/pricing.html">Pricing</a></li>
+			   <li><a href="{{ asset('front-theme-asset/gym') }}/contact.html">Contact</a></li>
 			 </ul>
-			  <script type="text/javascript" src="js/nav.js"></script>
+			  <script type="text/javascript" src="{{ asset('front-theme-asset/gym') }}/js/nav.js"></script>
 		  </div><!-- end h_menu4 -->
 		 <div class="clear"></div>
 	  </div>
@@ -100,7 +100,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<!-- start content-top -->
 			<div class="row content-top">
 				 <div class="col-md-5">
-				  <img src="images/pic.png" class="img-responsive" alt=""/> 
+				  <img src="{{ asset('front-theme-asset/gym') }}/images/pic.png" class="img-responsive" alt=""/>
 			     </div>
 				 <div class="col-md-7 content_left_text">
 				   <h3>Lorem ipsum dolor sit!</h3>
@@ -116,10 +116,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	 	    		<ul class="spinning">
 	 	    			<li class="live">live <span class="m_1">Spinning</span></li>
 	 	    			<li class="room">Room 2</li>
-	 	    			<div class="clear"></div>	
+	 	    			<div class="clear"></div>
 	 	    		</ul>
 					 <div class="view view-fifth">
-				  	   <img src="images/pic3.jpg" class="img-responsive" alt=""/>
+				  	   <img src="{{ asset('front-theme-asset/gym') }}/images/pic3.jpg" class="img-responsive" alt=""/>
 					      <div class="mask">
 	                       	<div class="info">Read More</div>
 			              </div>
@@ -129,10 +129,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	 	    		<ul class="spinning">
 	 	    			<li class="live">live <span class="m_1">Gym</span></li>
 	 	    			<li class="room">Room 1</li>
-	 	    			<div class="clear"></div>	
+	 	    			<div class="clear"></div>
 	 	    		</ul>
 					 <div class="view view-fifth">
-				  	   <img src="images/pic2.jpg" class="img-responsive"  alt=""/>
+				  	   <img src="{{ asset('front-theme-asset/gym') }}/images/pic2.jpg" class="img-responsive"  alt=""/>
 					      <div class="mask">
 	                       	<div class="info">Read More</div>
 			              </div>
@@ -142,10 +142,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 	 	    		<ul class="spinning">
 	 	    			<li class="live">live <span class="m_1">Pilates</span></li>
 	 	    			<li class="room">Room 4</li>
-	 	    			<div class="clear"></div>	
+	 	    			<div class="clear"></div>
 	 	    		</ul>
 					 <div class="view view-fifth">
-				  	   <img src="images/pic1.jpg" class="img-responsive" alt=""/>
+				  	   <img src="{{ asset('front-theme-asset/gym') }}/images/pic1.jpg" class="img-responsive" alt=""/>
 					      <div class="mask">
 	                       	<div class="info">Read More</div>
 			              </div>
@@ -293,8 +293,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			  <div class="col-md-4">
 		        <h3 class="m_2">Our Trainers</h3>
 		         <div class="course_demo">
-		          <ul id="flexiselDemo3">	
-					<li><img src="images/pic4.jpg" /><div class="desc">
+		          <ul id="flexiselDemo3">
+					<li><img src="{{ asset('front-theme-asset/gym') }}/images/pic4.jpg" /><div class="desc">
 						<h3>Lorem Ipsum<br><span class="m_text">Spinning</span></h3>
 						<p>Lorem ipsum dolor<br> sit amet, consectetuer.</p>
 						<div class="coursel_list">
@@ -307,7 +307,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						</div>
 						<div class="clear"></div>
 					</div></li>
-					<li><img src="images/pic5.jpg" /><div class="desc">
+					<li><img src="{{ asset('front-theme-asset/gym') }}/images/pic5.jpg" /><div class="desc">
 						<h3>Lorem Ipsum<br><span class="m_text">Kik Boxing</span></h3>
 						<p>Lorem ipsum dolor<br> sit amet, consectetuer.</p>
 						<div class="coursel_list">
@@ -319,8 +319,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<i class="fb"> </i>
 						</div>
 						<div class="clear"></div>
-					</div></li>	
-					<li><img src="images/pic4.jpg" /><div class="desc">
+					</div></li>
+					<li><img src="{{ asset('front-theme-asset/gym') }}/images/pic4.jpg" /><div class="desc">
 						<h3>Lorem Ipsum<br><span class="m_text">Spinning</span></h3>
 						<p>Lorem ipsum dolor<br> sit amet, consectetuer.</p>
 						<div class="coursel_list">
@@ -332,8 +332,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<i class="fb"> </i>
 						</div>
 						<div class="clear"></div>
-					</div></li>	
-					<li><img src="images/pic5.jpg" /><div class="desc">
+					</div></li>
+					<li><img src="{{ asset('front-theme-asset/gym') }}/images/pic5.jpg" /><div class="desc">
 						<h3>Lorem Ipsum<br><span class="m_text">Kik Boxing</span></h3>
 						<p>Lorem ipsum dolor<br> sit amet, consectetuer.</p>
 						<div class="coursel_list">
@@ -345,8 +345,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<i class="fb"> </i>
 						</div>
 						<div class="clear"></div>
-					</div></li>	
-					<li><img src="images/pic4.jpg" /><div class="desc">
+					</div></li>
+					<li><img src="{{ asset('front-theme-asset/gym') }}/images/pic4.jpg" /><div class="desc">
 						<h3>Lorem Ipsum<br><span class="m_text">Spinning</span></h3>
 						<p>Lorem ipsum dolor<br> sit amet, consectetuer.</p>
 						<div class="coursel_list">
@@ -358,7 +358,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<i class="fb"> </i>
 						</div>
 						<div class="clear"></div>
-					</div></li>							    	  	       	   	    	
+					</div></li>
 				</ul>
 				<script type="text/javascript">
 			$(window).load(function() {
@@ -366,28 +366,28 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					visibleItems: 4,
 					animationSpeed: 1000,
 					autoPlay: true,
-					autoPlaySpeed: 3000,    		
+					autoPlaySpeed: 3000,
 					pauseOnHover: true,
 					enableResponsiveBreakpoints: true,
-			    	responsiveBreakpoints: { 
-			    		portrait: { 
+			    	responsiveBreakpoints: {
+			    		portrait: {
 			    			changePoint:480,
 			    			visibleItems: 1
-			    		}, 
-			    		landscape: { 
+			    		},
+			    		landscape: {
 			    			changePoint:640,
 			    			visibleItems: 2
 			    		},
-			    		tablet: { 
+			    		tablet: {
 			    			changePoint:768,
 			    			visibleItems: 2
 			    		}
 			    	}
 			    });
-			    
+
 			});
 		</script>
-		<script type="text/javascript" src="js/jquery.flexisel.js"></script>
+		<script type="text/javascript" src="{{ asset('front-theme-asset/gym') }}/js/jquery.flexisel.js"></script>
 	  </div>
      </div>
      <div class="col-md-4">
@@ -396,7 +396,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
      	 	<div class="event-top">
 	     	 	<ul class="event1">
 	     	 		<h4>26 April, 2014</h4>
-	     	 		<img src="images/pic.jpg" alt=""/>
+	     	 		<img src="{{ asset('front-theme-asset/gym') }}/images/pic.jpg" alt=""/>
 	     	 	</ul>
 	     	 	<ul class="event1_text">
 	     	 		<span class="m_5">h.12.00-h.13.00</span>
@@ -411,7 +411,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
      	 	<div class="event-bottom">
 	     	 	<ul class="event1">
 	     	 		<h4>26 April, 2014</h4>
-	     	 		<img src="images/pic.jpg" alt=""/>
+	     	 		<img src="{{ asset('front-theme-asset/gym') }}/images/pic.jpg" alt=""/>
 	     	 	</ul>
 	     	 	<ul class="event1_text">
 	     	 		<span class="m_5">h.12.00-h.13.00</span>
@@ -429,7 +429,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
      	 <h3 class="m_2">From the blog</h3>
      	 <div class="blog_events">
      	 	<ul class="tab-left1">
-				<span class="tab1-img"><img src="images/pic7.jpg" alt=""></span>
+				<span class="tab1-img"><img src="{{ asset('front-theme-asset/gym') }}/images/pic7.jpg" alt=""></span>
 				<div class="tab-text1">
 				 <p><a href="#">nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip</a></p>
 				 <span class="m_date">25 April, 2014</span>
@@ -437,7 +437,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="clear"></div>
 			</ul>
 			<ul class="tab-left1">
-				<span class="tab1-img"><img src="images/pic6.jpg" alt=""></span>
+				<span class="tab1-img"><img src="{{ asset('front-theme-asset/gym') }}/images/pic6.jpg" alt=""></span>
 				<div class="tab-text1">
 				 <p><a href="#">soluta nobis eleifend option congue nihil imperdiet doming id</a></p>
 				 <span class="m_date">25 April, 2014</span>
@@ -445,7 +445,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				<div class="clear"></div>
 			</ul>
 			<ul class="tab-last1">
-				<span class="tab1-img"><img src="images/pic8.jpg" alt=""></span>
+				<span class="tab1-img"><img src="{{ asset('front-theme-asset/gym') }}/images/pic8.jpg" alt=""></span>
 				<div class="tab-text1">
 				 <p><a href="#">quod mazim placerat facer possim assum. Typi non habent</a></p>
 				 <span class="m_date">25 April, 2014</span>
@@ -518,12 +518,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		   <div class="col-md-4">
 		   	 <h3 class="m_2">Partner</h3>
 			  <ul class="partner">
-			  	<li><img src="images/p6.png" alt=""/></li>
-			  	<li><img src="images/p5.png" alt=""/></li>
-			  	<li><img src="images/p4.png" alt=""/></li>
-			  	<li><img src="images/p3.png" alt=""/></li>
-			  	<li><img src="images/p2.png" alt=""/></li>
-			  	<li><img src="images/p1.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p6.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p5.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p4.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p3.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p2.png" alt=""/></li>
+			  	<li><img src="{{ asset('front-theme-asset/gym') }}/images/p1.png" alt=""/></li>
 			  	 <div class="clear"></div>
 			  </ul>
 		   </div>
@@ -571,7 +571,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 				</div>
 				<div class="col-md-4">
 					<div class="f-logo">
-						<img src="images/logo.png" alt=""/>
+						<img src="{{ asset('front-theme-asset/gym') }}/images/logo.png" alt=""/>
 					</div>
 					<p class="m_9">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis</p>
 					<p class="address">Phone : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="m_10">(00) 222 666 444</span></p>
@@ -604,15 +604,15 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		    <div class="copy">
 		        <p>© 2014 Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
 		    </div>
-		    <div class="social">	
-			   <ul>	
+		    <div class="social">
+			   <ul>
 				  <li class="facebook"><a href="#"><span> </span></a></li>
 				  <li class="twitter"><a href="#"><span> </span></a></li>
-				  <li class="pinterest"><a href="#"><span> </span></a></li>	
+				  <li class="pinterest"><a href="#"><span> </span></a></li>
 				  <li class="google"><a href="#"><span> </span></a></li>
 				  <li class="tumblr"><a href="#"><span> </span></a></li>
-				  <li class="instagram"><a href="#"><span> </span></a></li>	
-				  <li class="rss"><a href="#"><span> </span></a></li>							
+				  <li class="instagram"><a href="#"><span> </span></a></li>
+				  <li class="rss"><a href="#"><span> </span></a></li>
 			   </ul>
 		    </div>
 		   <div class="clear"></div>
