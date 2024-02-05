ef72e59839d2596c8e3af74b20b4f2fe487b518e
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-05 10:35:22 +0330

    Work on left-sidebar Twenty Theme

--- resources/views/front/theme/twenty/left-sidebar.blade.php
+++ resources/views/front/theme/twenty/left-sidebar.blade.php
@@ -11,20 +11,20 @@
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
+		<script src="{{ asset('front-theme-asset/twenty') }}/js/jquery.min.js"></script>
+		<script src="{{ asset('front-theme-asset/twenty') }}/js/jquery.dropotron.min.js"></script>
+		<script src="{{ asset('front-theme-asset/twenty') }}/js/skel.min.js"></script>
+		<script src="{{ asset('front-theme-asset/twenty') }}/js/skel-layers.min.js"></script>
+		<script src="{{ asset('front-theme-asset/twenty') }}/js/init.js"></script>
 		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
+			<link rel="{{ asset('front-theme-asset/twenty') }}/stylesheet" href="css/skel.css" />
+			<link rel="{{ asset('front-theme-asset/twenty') }}/stylesheet" href="css/style.css" />
 		</noscript>
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
 	</head>
 	<body class="left-sidebar loading">
-	
+
 		<!-- Header -->
 			<header id="header">
 				<h1 id="logo"><a href="index.html">Twenty <span>by HTML5 UP</span></a></h1>
@@ -54,7 +54,7 @@
 					</ul>
 				</nav>
 			</header>
-		
+
 		<!-- Main -->
 			<article id="main">
 
@@ -63,13 +63,13 @@
 					<h2>Behold the <strong>Left Sidebar</strong></h2>
 					<p>Where things on the left ... accompany that on the right.</p>
 				</header>
-					
+
 				<!-- One -->
 					<section class="wrapper style4 container">
-					
+
 						<div class="row oneandhalf">
 							<div class="4u">
-								
+
 								<!-- Sidebar -->
 									<div class="sidebar">
 										<section>
@@ -100,11 +100,11 @@
 
 							</div>
 							<div class="8u skel-cell-important">
-								
+
 								<!-- Content -->
 									<div class="content">
 										<section>
-											<a href="#" class="image feature"><img src="images/pic02.jpg" alt="" /></a>
+											<a href="#" class="image feature"><img src="{{ asset('front-theme-asset/twenty') }}/images/pic02.jpg" alt="" /></a>
 											<header>
 												<h3>Dolore Amet Consequat</h3>
 											</header>
@@ -113,16 +113,16 @@
 											<p>In vestibulum massa quis arcu lobortis tempus. Nam pretium arcu in odio vulputate luctus. Suspendisse euismod lorem eget lacinia fringilla. Sed sed felis justo. Nunc sodales elit in laoreet aliquam. Nam gravida, nisl sit amet iaculis porttitor, risus nisi rutrum metus, non hendrerit ipsum arcu tristique est.</p>
 										</section>
 									</div>
-	
+
 							</div>
-						</div>					
+						</div>
 					</section>
 
 				<!-- Two -->
 					<section class="wrapper style1 container special">
 						<div class="row">
 							<div class="4u">
-							
+
 								<section>
 									<header>
 										<h3>This is Something</h3>
@@ -134,10 +134,10 @@
 										</ul>
 									</footer>
 								</section>
-							
+
 							</div>
 							<div class="4u">
-							
+
 								<section>
 									<header>
 										<h3>Also Something</h3>
@@ -149,10 +149,10 @@
 										</ul>
 									</footer>
 								</section>
-							
+
 							</div>
 							<div class="4u">
-							
+
 								<section>
 									<header>
 										<h3>Probably Something</h3>
@@ -164,16 +164,16 @@
 										</ul>
 									</footer>
 								</section>
-							
+
 							</div>
 						</div>
 					</section>
-					
+
 			</article>
 
 		<!-- Footer -->
 			<footer id="footer">
-			
+
 				<ul class="icons">
 					<li><a href="#" class="icon circle fa-twitter"><span class="label">Twitter</span></a></li>
 					<li><a href="#" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
@@ -181,9 +181,9 @@
 					<li><a href="#" class="icon circle fa-github"><span class="label">Github</span></a></li>
 					<li><a href="#" class="icon circle fa-dribbble"><span class="label">Dribbble</span></a></li>
 				</ul>
-				
+
 				<span class="copyright">&copy; Untitled. All rights reserved. Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
-			
+
 			</footer>
 
 	</body>
