b63ec770c8f4f938448f5aaebffef0aa04476404
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-06 11:05:32 +0330

    Update setting.blade.php

--- resources/views/front/theme/miral/setting.blade.php
+++ resources/views/front/theme/miral/setting.blade.php
@@ -107,22 +107,22 @@
                     </div>
                 @endif
             </div>
-            <div class="col-2 form-group">
+            <div class="col-1 form-group">
                 <label class="form-label">عنوان اول</label>
                 <input type="text" name="first_title_section2" class="form-control" placeholder="عنوان اول..."
                     value="{{ $settingModel->getSetting('first_title_section2', $account->id) }}">
             </div>
-            <div class="col-2 form-group">
+            <div class="col-1 form-group">
                 <label class="form-label">عنوان دوم</label>
                 <input type="text" name="secound_title_section2" class="form-control" placeholder="عنوان دوم..."
                     value="{{ $settingModel->getSetting('secound_title_section2', $account->id) }}">
             </div>
-            <div class="col-2 form-group">
+            <div class="col-1 form-group">
                 <label class="form-label">عنوان سوم</label>
                 <input type="text" name="thired_title_section2" class="form-control" placeholder="عنوان سوم..."
                     value="{{ $settingModel->getSetting('thired_title_section2', $account->id) }}">
             </div>
-            <div class="col-2 form-group">
+            <div class="col-1 form-group">
                 <label class="form-label">دکمه</label>
                 <input type="text" name="button_section2" class="form-control" placeholder="دکمه..."
                     value="{{ $settingModel->getSetting('button_section2', $account->id) }}">
