5481f6cb3f476dda13decee01fda5fed79d10d59
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-06 12:20:02 +0330

    Update index.blade.php

--- resources/views/front/theme/miral/index.blade.php
+++ resources/views/front/theme/miral/index.blade.php
@@ -209,13 +209,13 @@
                         has been.</p>
                 </div>
                 <div class="col-md-4">
-                    <img src="{{ asset(ert('tsp') . $settingModel->getSetting('first_icon', $accountId)) }}/images/flag.png" alt=""/>
+                    <img src="{{ asset(ert('tsp') . $settingModel->getSetting('secound_icon', $accountId)) }}/images/flag.png" alt=""/>
                     <h4><a href="index.html">MARKETING</a></h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum Lorem Ipsum
                         has been.</p>
                 </div>
                 <div class="col-md-4">
-                    <img {{ asset(ert('tsp') . $settingModel->getSetting('first_icon', $accountId)) }} alt=""/>
+                    <img {{ asset(ert('tsp') . $settingModel->getSetting('third_icon', $accountId)) }} alt=""/>
                     <h4><a href="index.html">RESEARCH</a></h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum Lorem Ipsum
                         has been.</p>
