3cbc2f80e4bbf66f2c33cb35abbd34a09d0bcf6a
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-07 18:03:34 +0330

    Update create.blade.php

--- resources/views/admin/nav/create.blade.php
+++ resources/views/admin/nav/create.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.master')
-@section('title', 'nav list')
+@section('title', 'ایجاد فهرست')
 @section('style')
     {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
     <style>
@@ -28,74 +28,67 @@
                     @csrf
                     <input type="hidden" id="action" name="action" value="{{ $action }}">
                     <input type="hidden" name="nav_id" value="{{ $nav?->id }}">
+                    @if ($errors->any())
+                        <div class="row">
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger">{{ $error }}</div>
+                            @endforeach
+                        </div>
+                    @endif
                     <div class="row">
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-body">
-                                    @if ($errors->any())
-                                        <div class="container">
-                                            <div class="row alert alert-danger">
-                                                <ul>
-                                                    @foreach ($errors->all() as $error)
-                                                        <li>{{ $error }}</li>
-                                                    @endforeach
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    @endif
-
                                     <div class="row">
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label class="required">نام <span class="text-danger">*</span></label>
-                                                <input type="text" name="name" class="form-control"
-                                                    value="{{ old('name') ?? $nav?->name }}" placeholder="نام..." required
-                                                    oninvalid="this.setCustomValidity('نام را وارد نمایید.')"
-                                                    oninput="this.setCustomValidity('')">
-                                            </div>
+                                        <div class="col-4 form-group">
+                                            <label class="required">نام <span class="text-danger">*</span></label>
+                                            <input type="text" name="name" class="form-control"
+                                                   value="{{ old('name') ?? $nav?->name }}" placeholder="نام..."
+                                                   required
+                                                   oninvalid="this.setCustomValidity('نام را وارد نمایید.')"
+                                                   oninput="this.setCustomValidity('')">
                                         </div>
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label class="required">برچسب <span class="text-danger">*</span></label>
-                                                <input type="text" name="label" class="form-control"
-                                                    value="{{ old('label') ?? $nav?->label }}" placeholder="برچسب..." required
-                                                    oninvalid="this.setCustomValidity('برچسب  را وارد نمایید.')"
-                                                    oninput="this.setCustomValidity('')">
-                                            </div>
+                                        <div class="col-4 form-group">
+                                            <label class="required">برچسب <span class="text-danger">*</span></label>
+                                            <input type="text" name="label" class="form-control"
+                                                   value="{{ old('label') ?? $nav?->label }}" placeholder="برچسب..."
+                                                   required
+                                                   oninvalid="this.setCustomValidity('برچسب  را وارد نمایید.')"
+                                                   oninput="this.setCustomValidity('')">
                                         </div>
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label>وضعیت</label>
-                                                <select name="status" class="form-control">
-                                                    <option value="فعال" @selected($nav?->status == 'فعال')> فعال</option>
-                                                    <option value="غیرفعال" @selected($nav?->status == 'غیرفعال')>غیرفعال</option>
-                                                </select>
-                                            </div>
+                                        <div class="col-4 form-group">
+                                            <label>وضعیت</label>
+                                            <select name="status" class="form-control">
+                                                <option value="فعال" @selected($nav?->status == 'فعال')> فعال</option>
+                                                <option value="غیرفعال" @selected($nav?->status == 'غیرفعال')>غیرفعال
+                                                </option>
+                                            </select>
                                         </div>
+                                    </div>
+                                    <div class="row">
                                         <div class="col-12">
                                             <div class="form-group abstract">
                                                 <label>توضیحات</label>
-                                                <textarea name="desc" class="form-control Reditor1" placeholder="توضیحات ...">{{ old('desc') ?? $nav?->desc }}</textarea>
+                                                <textarea name="desc" class="form-control Reditor1"
+                                                          placeholder="توضیحات ...">{{ old('desc') ?? $nav?->desc }}</textarea>
                                             </div>
                                         </div>
-                                        <div class="col-12">
-                                            <button class="btn btn-success btn-sm">ذخیره</button>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="row">
+                                        <div class="col-12 text-left">
+                                            <button class="btn btn-success">ذخیره</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-
                     </div>
-
                 </form>
             </div>
         </section>
-        <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
 @endsection
 @section('scripts')
     <script src="{{ asset('js/ckeditor.js') }}"></script>
