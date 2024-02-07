aa6b3f901c6672b199f510ad99b6b41ebbaeac69
MohammadRasoulZarei <rasoul16and17@gmail.com>
2024-02-07 11:50:50 +0330

    Add active theme button and styling.

--- resources/views/admin/theme/chooseTheme.blade.php
+++ resources/views/admin/theme/chooseTheme.blade.php
@@ -13,6 +13,34 @@
                         <div class="card-body">
                             <div class="row">
                                 @foreach ($themes as $theme )
+                               @if ($account->activeTheme() == $theme->name)
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        @if ($theme->preview)
+                                            <img class="w-100 object-fit-contain" src="{{ asset(ert('theme-path') . $theme->preview) }}" alt="">
+                                        @else
+                                            <img class="card-img-top" src="{{ asset('images/no-img-theme.jpg') }}" alt="Card image cap">
+                                        @endif
+                                        <div class="card-body">
+                                            <h5 class="card-title">{{ $theme->label }}</h5>
+                                            <p class="card-text">{{ $theme->slogan }}</p>
+                                            @if ($account->activeTheme() == $theme->name)
+                                                <button class="btn btn-success">فعال شده</button>
+                                            @else
+
+                                                <a href="{{ route('theme.activeTheme', $theme->name) }}" class="btn btn-primary">فعالسازی</a>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                                @break
+                                @endif
+
+                                @endforeach
+                                @foreach ($themes as $theme )
+                                @if ($account->activeTheme() == $theme->name)
+                                @continue
+                                @endif
                                     <div class="col-md-3">
                                         <div class="card">
                                             @if ($theme->preview)
@@ -26,6 +54,7 @@
                                                 @if ($account->activeTheme() == $theme->name)
                                                     <button class="btn btn-success">فعال شده</button>
                                                 @else
+
                                                     <a href="{{ route('theme.activeTheme', $theme->name) }}" class="btn btn-primary">فعالسازی</a>
                                                 @endif
                                             </div>
