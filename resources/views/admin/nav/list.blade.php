66985129085a2978eaabb1c18618f19880bb9774
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-07 16:48:13 +0330

    Update list.blade.php

--- resources/views/admin/nav/list.blade.php
+++ resources/views/admin/nav/list.blade.php
@@ -1,11 +1,11 @@
 @extends('admin.master')
-@section('title', 'Page List')
+@section('title', 'فهرست ها')
 @section('content')
     @include('sweetalert::alert')
     @include('admin.partial.nav')
     @include('admin.partial.aside')
     <div class="content-wrapper">
-        {{ breadcrumb('لسیت فهرست ها') }}
+        {{ breadcrumb('فهرست ها') }}
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
@@ -13,65 +13,51 @@
                         <div class="card">
                             <div class="card-header d-flex justify-content-between">
                                 <h3 class="card-title">لیست فهرست ها ({{ $navs->count() }})</h3>
-                                <a href="{{ route('nav.create') }}" class="d-flex align-items-center btn btn-success btn-sm mr-auto text-white">
-                                    <i class="fa fa-plus ml-2"></i> افزودن فهرست
+                                <a href="{{ route('nav.create') }}" class="d-flex align-items-center btn btn-info btn-sm mr-auto">
+                                    جدید
                                 </a>
                             </div>
-                            <div class="card-body">
+                            <div class="card-body p-0">
                                 @if ($navs->isEmpty())
                                     <div class="d-flex justify-content-center">
                                         <span class="not-found">فهرستی یافت نشد.</span>
                                     </div>
                                 @else
-                                    <table class="table table-bordered table-striped table-hover">
-                                        <thead>
+                                    <table class="table table-bordered table-striped text-center table-hover">
+                                        <tr class="table-warning">
+                                            <th>#</th>
+                                            <th>نام</th>
+                                            <th>برچسب</th>
+                                            <th>توضیحات</th>
+                                            <th>وضعیت</th>
+                                            <th>عملیات</th>
+                                        </tr>
+                                        @foreach ($navs as $key => $nav)
                                             <tr>
-                                                <th>#</th>
-                                                <th>نام</th>
-                                                <th>برچسب</th>
-                                                <th>توضیحات</th>
-                                                <th>وضعیت</th>
-                                                <th width="10%">عملیات</th>
+                                                <td>{{ 1 + $key }}</td>
+                                                <td>{{ $nav->name }}</td>
+                                                <td>{{ $nav->label }}</td>
+                                                <td>{!! $nav->desc !!}</td>
+                                                <td>{{ $nav->status }}</td>
+                                                <td class="d-flex">
+                                                    <a class="btn btn-warning btn-sm ml-2 d-flex align-items-center" href="{{ route('nav.create', ['action' => 'update', 'nav_id' => $nav->id]) }}" data-toggle="tooltip" data-placement="top" title="ویرایش"><i class="fa fa-edit"></i></a>
+                                                    <a class="btn btn-danger btn-sm ml-2 d-flex align-items-center" data-confirm-delete='true' href="{{ route('nav.destroy',$nav->id ) }}" data-toggle="tooltip" data-placement="top" title="حذف"><i class="fa fa-trash"></i></a>
+                                                </td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($navs as $key => $nav)
-                                                <tr>
-                                                    <td>{{ 1 + $key }}</td>
-
-
-                                                    <td>{{ $nav->name }}</td>
-                                                    <td>{{ $nav->label }}</td>
-                                                    <td>{!! $nav->desc !!}</td>
-                                                    <td>{{ $nav->status }}</td>
-
-                                                    <td>
-                                                        <div class="d-flex">
-
-                                                            <a class="btn btn-warning btn-sm ml-2 d-flex align-items-center" href="{{ route('nav.create', ['action' => 'update', 'nav_id' => $nav->id]) }}" data-toggle="tooltip" data-placement="top" title="ویرایش"><i class="fa fa-edit"></i></a>
-                                                            <a class="btn btn-danger btn-sm ml-2 d-flex align-items-center" data-confirm-delete='true' href="{{ route('nav.destroy',$nav->id ) }}" data-toggle="tooltip" data-placement="top" title="حذف"><i class="fa fa-trash"></i></a>
-
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    @endif
-                                </div>
-                                <div class="w-100 mt-3 d-flex justify-content-center"></div>
+                                        @endforeach
+                                    </table>
+                                @endif
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     </div>
 @endsection
-
 @section('scripts')
-
     <script type="text/javascript">
-        $(document).on('click', '.delete-confirm', function() {
+        $(document).on("click", ".delete-confirm", function () {
             var form =  $(this).closest("form");
             event.preventDefault();
             new swal({
@@ -79,7 +65,7 @@
                 text:  "آیا از حذف نوشته مطمئن هستید؟",
                 icon: "warning",
                 dangerMode: true,
-                buttons: ["خیر","بله"],
+                buttons: ["خیر", "بله"],
             }).then((willDelete) => {
                 if (willDelete) {
                     form.submit();
