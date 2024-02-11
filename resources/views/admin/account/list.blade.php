0e24fc83283530b4fdc0e6f62fe3c0f2c3639b91
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-11 22:53:13 +0330

    Update list.blade.php

--- resources/views/admin/account/list.blade.php
+++ resources/views/admin/account/list.blade.php
@@ -49,8 +49,7 @@
                                 @if (count($accounts) > 0)
                                     <div class="table-responsive">
                                         <table class="table table-bordered table-hover text-center">
-                                            <thead>
-                                            <tr>
+                                            <tr class="table-warning">
                                                 <th class="px-4">#</th>
                                                 <th class="px-4">نوع اکانت</th>
                                                 <th class="px-4">نام</th>
@@ -60,93 +59,84 @@
                                                 <th class="px-4">دلیل غیرفعال سازی</th>
                                                 <th class="px-4">عملیات</th>
                                             </tr>
-                                            </thead>
                                             <tbody>
-                                            @foreach ($accounts as $account)
-                                                <tr>
-                                                    <td class="w-auto text-center">{{ fa_number($loop->index + 1) }}
-                                                    </td>
-                                                    <td class="w-auto text-center">{{ $account->account_type }}</td>
-                                                    <td class="w-auto text-center">{{ $account->name }}</td>
-                                                    <td class="w-auto text-center">{{ $account->family }}</td>
-                                                    <td class="w-auto text-center">{{ fa_number($account->mobile) }}
-                                                    </td>
-                                                    <td class="w-auto text-center">
-                                                        @if ($account->account_status == 'active')
-                                                            <span class="badge bg-success"
-                                                                  style="font-size: 17px;color: #FFF !important;">فعال</span>
-                                                        @elseif ($account->account_status == 'waiting')
-                                                            <span class="badge bg-warning"
-                                                                  style="font-size: 17px;color: #FFF !important;">در
-                                                                    انتظار تایید</span>
-                                                        @elseif ($account->account_status == 'deActive')
-                                                            <span class="badge bg-danger"
-                                                                  style="font-size: 17px;color: #FFF !important;">غیرفعال</span>
+                                                @foreach ($accounts as $account)
+                                                    <tr>
+                                                        <td>{{ fa_number($loop->index + 1) }}</td>
+                                                        <td>{{ $account->account_type }}</td>
+                                                        <td>{{ $account->name }}</td>
+                                                        <td>{{ $account->family }}</td>
+                                                        <td>{{ fa_number($account->mobile) }}</td>
+                                                        <td>
+                                                            @if ($account->account_status == 'active')
+                                                                <span class="badge bg-success" style="font-size: 17px;color: #FFF !important;">فعال</span>
+                                                            @elseif ($account->account_status == 'waiting')
+                                                                <span class="badge bg-warning" style="font-size: 17px;color: #FFF !important;">درانتظار تایید</span>
+                                                            @elseif ($account->account_status == 'deActive')
+                                                                <span class="badge bg-danger" style="font-size: 17px;color: #FFF !important;">غیرفعال</span>
+                                                            @endif
+                                                        </td>
+                                                        @if ($account->deactivation_reason)
+                                                            <td>{{ $account->deactivation_reason }}</td>
+                                                        @else
+                                                            <td class="w-auto text-center">------</td>
                                                         @endif
-
-                                                    </td>
-                                                    @if ($account->deactivation_reason)
-                                                        <td class="w-auto text-center">
-                                                            {{ $account->deactivation_reason }} </td>
-                                                    @else
-                                                        <td class="w-auto text-center"> ------</td>
-                                                    @endif
-                                                    <td class="d-flex">
-                                                        <a href="{{ route('user.showUsers', ['accountId' => $account->id]) }}"
-                                                           class="btn btn-primary m-1">مشاهده کاربران</a>
-                                                        <a href="{{ route('account.edit', $account->id) }}"
-                                                           class="btn btn-warning m-1">ویرایش</a>
-                                                        <form action="{{ route('account.destroy', $account->id) }}"
-                                                              method="POST" style="display: inline-block;">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit" class="btn btn-danger btn-sm m-1"
-                                                                    id="confirmdelete{{ $account->id }}">حذف
-                                                            </button>
-                                                        </form>
-                                                        @if ($account->account_status == 'active')
-                                                            <button type="button" name="active"
-                                                                    class="btn btn-danger btn-sm Deactive-button"
-                                                                    data-toggle="modal" data-target="#exampleModal"
-                                                                    id="{{ $account->id }}" value="deActive"
-                                                                    style="height: 39px; margin-top: 4px;">غیرفعال
-                                                                سازی
-                                                            </button>
-                                                        @elseif ($account->account_status == 'deActive')
-                                                            <form action="{{ route('account.activation') }}"
-                                                                  class="mt-1  m-1" method="POST">
-                                                                @csrf
-                                                                @method('PUT')
-                                                                <input type="hidden" name="id"
-                                                                       value="{{ $account->id }}">
-                                                                <button type="submit" name="active"
-                                                                        class="btn btn-success btn-sm" value="active">
-                                                                    فعال سازی
-                                                                </button>
-                                                            </form>
-                                                        @elseif ($account->account_status == 'waiting')
-                                                            <button type="button" name="active"
-                                                                    class="btn btn-danger btn-sm Deactive-button"
-                                                                    data-toggle="modal" data-target="#exampleModal"
-                                                                    id="{{ $account->id }}" value="deActive"
-                                                                    style="height: 39px; margin-top: 4px;">غیرفعال
-                                                                سازی
-                                                            </button>
-                                                            <form action="{{ route('account.activation') }}"
-                                                                  class="mt-1  m-1" method="POST">
-                                                                @csrf
-                                                                @method('PUT')
-                                                                <input type="hidden" name="id"
-                                                                       value="{{ $account->id }}">
-                                                                <button type="submit" name="active"
-                                                                        class="btn btn-success btn-sm" value="active">
-                                                                    فعال سازی
-                                                                </button>
-                                                            </form>
-                                                        @endif
-                                                    </td>
-                                                </tr>
-                                            @endforeach
+                                                        <td class="d-flex">
+                                                            <a href="{{ route('user.showUsers', ['accountId' => $account->id]) }}"
+                                                                class="btn btn-primary btn-sm mx-1">مشاهده کاربران</a>
+                                                            <a href="{{ route('account.edit', $account->id) }}"
+                                                                class="btn btn-warning btn-sm mx-1"><i class="fa fa-edit"></i></a>
+                                                                <form action="{{ route('account.destroy', $account->id) }}"
+                                                                    method="POST" style="display: inline-block;">
+                                                                        @csrf
+                                                                        @method('DELETE')
+                                                                    <button type="submit" class="btn btn-danger btn-sm mx-1"
+                                                                            id="confirmdelete{{ $account->id }}"><i class="fa fa-trash"></i>
+                                                                    </button>
+                                                                </form>
+                                                                @if ($account->account_status == 'active')
+                                                                    <button type="button" name="active"
+                                                                            class="btn btn-danger btn-sm Deactive-button"
+                                                                            data-toggle="modal" data-target="#exampleModal"
+                                                                            id="{{ $account->id }}" value="deActive"
+                                                                            style="height: 39px; margin-top: 4px;">غیرفعال
+                                                                        سازی
+                                                                    </button>
+                                                                @elseif ($account->account_status == 'deActive')
+                                                                    <form action="{{ route('account.activation') }}"
+                                                                        class="mt-1  m-1" method="POST">
+                                                                        @csrf
+                                                                        @method('PUT')
+                                                                        <input type="hidden" name="id"
+                                                                               value="{{ $account->id }}">
+                                                                        <button type="submit" name="active"
+                                                                                class="btn btn-success btn-sm" value="active">
+                                                                            فعال سازی
+                                                                        </button>
+                                                                    </form>
+                                                                @elseif ($account->account_status == 'waiting')
+                                                                    <button type="button" name="active"
+                                                                            class="btn btn-danger btn-sm Deactive-button"
+                                                                            data-toggle="modal" data-target="#exampleModal"
+                                                                            id="{{ $account->id }}" value="deActive"
+                                                                            style="height: 39px; margin-top: 4px;">غیرفعال
+                                                                        سازی
+                                                                    </button>
+                                                                    <form action="{{ route('account.activation') }}"
+                                                                          class="mt-1  m-1" method="POST">
+                                                                        @csrf
+                                                                        @method('PUT')
+                                                                        <input type="hidden" name="id"
+                                                                               value="{{ $account->id }}">
+                                                                        <button type="submit" name="active"
+                                                                                class="btn btn-success btn-sm" value="active">
+                                                                            فعال سازی
+                                                                        </button>
+                                                                    </form>
+                                                                @endif
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
                                             </tbody>
                                         </table>
                                     </div>
@@ -158,12 +148,7 @@
                     </div>
                 </div>
             </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </section>
-    <!-- /.content -->
+        </section>
     </div>
     <!-- /.content-wrapper -->
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
@@ -197,12 +182,8 @@
             </div>
         </div>
     </div>
-
 @endsection
-
-
 @section('scripts')
-
     @if ($accounts->count() > 0)
         @foreach ($accounts as $account)
             <script>
