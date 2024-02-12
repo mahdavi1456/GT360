7f8919736651aa66668378312e949a4313015acd
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-12 23:32:36 +0330

    Update IPG.php

--- app/Servieses/IPG.php
+++ app/Servieses/IPG.php
@@ -83,7 +83,7 @@ class IPG
         curl_close($ch);
         $result = json_decode($result, true);
 
-        dd($result->errors);
+        dd($result['errors']);
 
         $message = $result['errors']['message'];
         $code = $result['errors']['code'];
