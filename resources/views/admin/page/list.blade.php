f705f2711d8acd5a52c31a23f52bf79ac394c433
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-12 19:56:57 +0330

    Update list.blade.php

--- resources/views/admin/page/list.blade.php
+++ resources/views/admin/page/list.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.master')
-@section('title', 'Page List')
+@section('title', 'صفحات')
 @section('content')
     @include('sweetalert::alert')
     @include('admin.partial.nav')
@@ -62,78 +62,65 @@
                                     <i class="fa fa-plus ml-2"></i> افزودن صفحه
                                 </a>
                             </div>
-                            <div class="card-body">
+                            <div class="card-body p-0 table-responsive">
                                 @if ($pages->isEmpty())
-                                    <div class="d-flex justify-content-center">
-                                        <span class="not-found">صفحه یافت نشد.</span>
-                                    </div>
+                                    <div class="alert alert-danger m-2">موردی جهت نمایش موجود نیست.</div>
                                 @else
-                                    <table class="table table-bordered table-striped table-hover">
-                                        <thead>
+                                    <table class="table table-bordered table-striped table-hover text-center">
+                                        <tr class="table-warning">
+                                            <th>#</th>
+                                            <th>تصویر شاخص</th>
+                                            <th>عنوان</th>
+                                            <th>بازدید</th>
+                                            <th>نویسنده</th>
+                                            <th>تاریخ</th>
+                                            <th>وضعیت</th>
+                                            <th>عملیات</th>
+                                        </tr>
+                                        @foreach ($pages as $key => $page)
                                             <tr>
-                                                <th>#</th>
-                                                <th>تصویر شاخص</th>
-
-                                                <th>عنوان</th>
-                                                <th>بازدید</th>
-                                                <th>نویسنده</th>
-                                                <th>تاریخ</th>
-                                                <th>وضعیت</th>
-                                                <th width="10%">عملیات</th>
+                                                <td>{{ $pages->firstItem() + $key }}</td>
+                                                <td>
+                                                    @if ($page->thumbnail)
+                                                        <img style="width:100px !important; object-fit: contain" src="{{ asset(ert('pip')) . '/' . $page->thumbnail }}">
+                                                    @else
+                                                        <span class="badge bg-danger">بدون تصویر</span>
+                                                    @endif
+                                                </td>
+                                                <td>{{ $page->title }}</td>
+                                                {{-- <td>{{ $post->visitLogs()->count() }}</td> --}}
+                                                <td>{{ 25 }}</td>
+                                                <td>{{ ($page->author_object->name . ' ' . $page->author_object->family) }}</td>
+                                                <td>{{ zaman(($page->created_at)) }}</td>
+                                                <td>
+                                                    <span class="badge {{ $page->publish_status == 'publish' ? 'badge-success' :'badge-danger'  }}">{{ $page->publish_status =='publish' ? 'انتشار': 'عدم انتشار'  }}</span>
+                                                </td>
+                                                <td>
+                                                    <div class="d-flex">
+                                                        <a class="btn btn-info btn-sm ml-2" target="_blank" href="">مشاهده</a>
+                                                        <a class="btn btn-warning btn-sm ml-2 d-flex align-items-center" href="{{ route('page.create', ['action' => 'update', 'page' => $page->id]) }}" data-toggle="tooltip" data-placement="top" title="ویرایش"><i class="fa fa-edit"></i></a>
+                                                        <div class="display-inline-block">
+                                                            <form method="post" class="h-100" action="{{ route('page.destroy', $page->id) }}">
+                                                                @csrf
+                                                                <input type="hidden" name="_method" value="DELETE">
+                                                                <button type="submit" data-toggle="tooltip" data-placement="top" title="حذف" class="h-100 delete-confirm btn btn-danger btn-sm d-flex align-items-center"><i class="fa fa-close"></i></button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </td>
                                             </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($pages as $key => $page)
-                                                <tr>
-                                                    <td>{{ $pages->firstItem() + $key }}</td>
-                                                    <td>
-                                                        <div class="text-center">
-                                                            @if ($page->thumbnail)
-                                                                <img  style="width:100px !important; object-fit: contain" src="{{ asset(ert('pip')) . '/' . $page->thumbnail }}">
-                                                                @else
-                                                                فاقد تصویر
-                                                            @endif
-                                                        </div>
-                                                    </td>
-
-                                                    <td>{{ $page->title }}</td>
-                                                    {{-- <td>{{ $post->visitLogs()->count() }}</td> --}}
-                                                    <td>{{ 25 }}</td>
-                                                    <td>{{ ($page->author_object->name . ' ' . $page->author_object->family) }}</td>
-                                                    <td>{{ zaman(($page->created_at)) }}</td>
-                                                    <td>
-                                                        <span class="badge {{ $page->publish_status == 'publish' ? 'badge-success' :'badge-danger'  }}">{{ $page->publish_status =='publish' ? 'انتشار': 'عدم انتشار'  }}</span>
-                                                    </td>
-                                                    <td>
-                                                        <div class="d-flex">
-                                                            <a class="btn btn-info btn-sm ml-2" target="_blank" href="">مشاهده</a>
-                                                            <a class="btn btn-warning btn-sm ml-2 d-flex align-items-center" href="{{ route('page.create', ['action' => 'update', 'page' => $page->id]) }}" data-toggle="tooltip" data-placement="top" title="ویرایش"><i class="fa fa-edit"></i></a>
-                                                            <div class="display-inline-block">
-                                                                <form method="post" class="h-100" action="{{ route('page.destroy', $page->id) }}">
-                                                                    @csrf
-                                                                    <input type="hidden" name="_method" value="DELETE">
-                                                                    <button type="submit" data-toggle="tooltip" data-placement="top" title="حذف" class="h-100 delete-confirm btn btn-danger btn-sm d-flex align-items-center"><i class="fa fa-close"></i></button>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                            </tbody>
-                                        </table>
-                                    @endif
-                                </div>
-                                <div class="w-100 mt-3 d-flex justify-content-center"></div>
+                                        @endforeach
+                                    </table>
+                                @endif
                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
     </div>
 @endsection
-
 @section('scripts')
-
     <script type="text/javascript">
         $(document).on('click', '.delete-confirm', function() {
             var form =  $(this).closest("form");
