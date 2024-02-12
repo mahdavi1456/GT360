19bc118c60328a8c1481bdb8e4bf803e7190027c
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-12 14:47:49 +0330

    Update setting.blade.php

--- resources/views/front/theme/senkadagala/setting.blade.php
+++ resources/views/front/theme/senkadagala/setting.blade.php
@@ -4,14 +4,15 @@
     </div>
     <div class="card-body">
         <div class="row">
-            <div class="col-6 form-group">
-                <label class="form-label">تصویر کاور دوم</label>
-                <input type="file" name="second_cover" onchange="uploadImage(this)">
-                @if ($image = imageLoader('second_cover'))
+          <div class="col-6 form-group">
+                <label class="form-label">تصویر لوگو</label>
+                <input type="file" name="first_cover" onchange="uploadImage(this)">
+
+                @if ($image = imageLoader('first_cover'))
                     <div class="imageLoader position-relative">
                         <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
-                        <button type="button" onclick="destroyImage('second_cover')"
-                            class="btn btn-sm btn-danger position-absolute" style="bottom: 0; left: 49%">حذف</button>
+                        <button type="button" onclick="destroyImage('first_cover')" class="btn btn-sm btn-danger position-absolute"
+                            style="bottom: 0; left: 49%">حذف</button>
                     </div>
                 @endif
             </div>
