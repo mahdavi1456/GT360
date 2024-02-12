1cfe7c837264fa0b4bcf5cfbd94706a7c409a330
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-12 18:27:06 +0330

    Update selectNavs.blade.php

--- resources/views/admin/theme/selectNavs.blade.php
+++ resources/views/admin/theme/selectNavs.blade.php
@@ -1,68 +1,57 @@
 @extends('admin.master')
-@section('title', 'Category List')
+@section('title', 'تخصیص فهرست به قالب')
 @section('content')
     @include('sweetalert::alert')
     @include('admin.partial.nav')
     @include('admin.partial.aside')
-    <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        {{ breadcrumb('انتخاب فهرست') }}
-        <!-- Main content -->
+        {{ breadcrumb('تخصیص فهرست به قالب') }}
         <section class="content">
             <div class="container-fluid">
+                <div class="row">
+                    @if ($errors->any())
+                        <div class="col-12">
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger">{{ $error }}</div>
+                            @endforeach
+                        </div>
+                    @endif
+                </div>
                 <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                @if ($errors->any())
-                                    <div class="container">
-                                        <div class="row alert alert-danger">
-                                            <ul>
-                                                @foreach ($errors->all() as $error)
-                                                    <li>{{ $error }}</li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    </div>
-                                @endif
-                                <form  action="{{ route('theme.navStore', $theme->id) }}" method="POST">
+                    <form  action="{{ route('theme.navStore', $theme->id) }}" method="POST">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
                                     @csrf
-                                    <label for=""> فهرست ها</label>
+                                    <label> فهرست ها</label>
                                     <select name="navs[]" class="custom-select select2" multiple>
                                         <option value="">انتخاب کنید...</option>
                                         @foreach ($navs as $nav)
                                             <option @selected(in_array($nav->id,$pluck)) value="{{ $nav->id }}">{{ $nav->name }}</option>
                                         @endforeach
-
                                     </select>
-                                    <div class="col-12 text-center mt-3">
-                                        <button type="submit" class="btn btn-primary">اعمال</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="col-md-6">
-                                <label for=""> لیست فهرست ها</label>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item list-group-item-action active">
-                                        <strong>فهرست های مربوط به قالب {{ $theme->name }}</strong>
-                                    </li>
-                                    @foreach ($themNavs as $nav)
-                                        <li class="list-group-item">{{ $nav->name }}</li>
-                                    @endforeach
-                                </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <label> لیست فهرست ها</label>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item list-group-item-action active">
+                                            <strong>فهرست های مربوط به قالب {{ $theme->name }}</strong>
+                                        </li>
+                                        @foreach ($themNavs as $nav)
+                                            <li class="list-group-item">{{ $nav->name }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                        <div class="card-footer text-left">
+                            <button type="submit" class="btn btn-success">ذخیره</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </section>
-        <!-- /.content -->
     </div>
-    <!-- /.content-wrapper -->
-
 @endsection
-
 @section('scripts')
-
 @endsection
