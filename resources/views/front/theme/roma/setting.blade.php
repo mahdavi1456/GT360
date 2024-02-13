87f25a6ce10cdce3ad35fdce905e19407b22be63
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-13 14:59:09 +0330

    Update setting.blade.php

--- resources/views/front/theme/roma/setting.blade.php
+++ resources/views/front/theme/roma/setting.blade.php
@@ -241,38 +241,40 @@
                         <input type="text" name="title_team" class="form-control" placeholder="عنوان..."
                             value="{{ $settingModel->getSetting('title_team', $account->id) }}">
                     </div>
-                    
-                        <label class="form-label">توضیحات</label>
-                        <input type="text" name="description_team" class="form-control" placeholder="توضیحات..."
-                            value="{{ $settingModel->getSetting('description_team', $account->id) }}">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 form-group ">
-                        <label class="form-label ">تصویر اول</label>
-                        <input type="file" name="team_image1" onchange="uploadImage(this)">
-                        @if ($image = imageLoader('team_image1'))
-                            <div class="imageLoader position-relative">
-                                <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
-                                <button type="button" onclick="destroyImage('team_image1')"
-                                    class="btn btn-sm btn-danger position-absolute"
-                                    style="bottom: 0; left: 49%">حذف</button>
-                            </div>
-                        @endif
-                    </div>
-                    <div class="col-3 form-group">
-                        <label class="form-label">عنوان تصویر اول</label>
-                        <input type="text" name="title_team_image1" class="form-control"
-                            placeholder="آیکن اول..."
-                            value="{{ $settingModel->getSetting('title_team_image1', $account->id) }}">
-                        <hr>
-                        <label class="form-label">زیر عنوان تصویر اول</label>
-                        <input type="text" name="subtitle_team_image1" class="form-control"
-                            placeholder="عنوان اول..."
-                            value="{{ $settingModel->getSetting('subtitle_team_image1', $account->id) }}">
-                    </div>
+
+                    <label class="form-label">توضیحات</label>
+                    <input type="text" name="description_team" class="form-control" placeholder="توضیحات..."
+                        value="{{ $settingModel->getSetting('description_team', $account->id) }}">
                 </div>
             </div>
+            <div class="row">
+                <div class="col-12 form-group ">
+                    <label class="form-label ">تصویر اول</label>
+                    <input type="file" name="team_image1" onchange="uploadImage(this)">
+                    @if ($image = imageLoader('team_image1'))
+                        <div class="imageLoader position-relative">
+                            <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
+                            <button type="button" onclick="destroyImage('team_image1')"
+                                class="btn btn-sm btn-danger position-absolute"
+                                style="bottom: 0; left: 49%">حذف</button>
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3 form-group">
+                    <label class="form-label">عنوان تصویر اول</label>
+                    <input type="text" name="title_team_image1" class="form-control"
+                        placeholder="عنوان تصویر اول..."
+                        value="{{ $settingModel->getSetting('title_team_image1', $account->id) }}">
+                    <hr>
+                    <label class="form-label">زیر عنوان تصویر اول</label>
+                    <input type="text" name="subtitle_team_image1" class="form-control"
+                        placeholder="زیرعنوان تصویر اول..."
+                        value="{{ $settingModel->getSetting('subtitle_team_image1', $account->id) }}">
+                </div>
+            </div>
+
     @endif
 </div>
 
