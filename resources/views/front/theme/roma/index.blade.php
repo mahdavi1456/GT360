99e6e319859ea9305cfd7e08c3bb9ed960ab12e2
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-13 14:00:50 +0330

    Update index.blade.php

--- resources/views/front/theme/roma/index.blade.php
+++ resources/views/front/theme/roma/index.blade.php
@@ -24,7 +24,7 @@
     </div>
     <!-- home section -->
     <section id="home"
-        style="background: url(' {{ asset(ert('tsp') . $settingModel->getSetting('background_cover', $accountId)) }}') cover no-repeat  fixed;">
+        style="background: url(' {{ asset(ert('tsp') . $settingModel->getSetting('background_cover', $accountId)) }}') 100% 0 repeat-y fixed;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-sm-12">
