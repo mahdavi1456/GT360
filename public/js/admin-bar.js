c95aea23cdcdf6afc33881f0cd54d47b15b4a8f5
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-13 19:38:03 +0330

    Update admin-bar.js

--- public/js/admin-bar.js
+++ public/js/admin-bar.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $(document.body).on("click", "#save-admin-bar-setting", function () {
+jQuery(document).ready(function () {
+    jQuery(document.body).on("click", "#save-admin-bar-setting", function () {
         alert("yes");
     });
 });
