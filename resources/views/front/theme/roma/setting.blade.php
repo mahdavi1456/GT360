2fc3b60bfd13129819b7265e170107ac2d011684
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-13 13:32:18 +0330

    Update setting.blade.php

--- resources/views/front/theme/roma/setting.blade.php
+++ resources/views/front/theme/roma/setting.blade.php
@@ -225,36 +225,45 @@
         </div>
     @endif
 </div>
-
 <div class="card card-warning">
-    <div class="card-header">
+    <div class="card-header pull-right">
         <h3 class="card-title">تصاویر</h3>
+        <select name="image_status" class="form-select pull-left" onchange="this.form.submit()">
+            <option {{ $settingModel->getSetting('image_status', $account->id) == 1 ? 'selected' : '' }}
+                value="1">فعال</option>
+            <option {{ $settingModel->getSetting('image_status', $account->id) == 0 ? 'selected' : '' }}
+                value="0">غیرفعال</option>
+        </select>
     </div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-6 form-group">
-                <label class="form-label">تصویر کاور اول</label>
-                <input type="file" name="first_cover" onchange="uploadImage(this)">
-                @if ($image = imageLoader('first_cover'))
-                    <div class="imageLoader position-relative">
-                        <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
-                        <button type="button" onclick="destroyImage('first_cover')"
-                            class="btn btn-sm btn-danger position-absolute" style="bottom: 0; left: 49%">حذف</button>
-                    </div>
-                @endif
-            </div>
-            <div class="col-6 form-group">
-                <label class="form-label">تصویر کاور دوم</label>
-                <input type="file" name="second_cover" onchange="uploadImage(this)">
+    @if ($settingModel->getSetting('image_status', $account->id) == 1)
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6 form-group">
+                    <label class="form-label">تصویر کاور اول</label>
+                    <input type="file" name="first_cover" onchange="uploadImage(this)">
+                    @if ($image = imageLoader('first_cover'))
+                        <div class="imageLoader position-relative">
+                            <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
+                            <button type="button" onclick="destroyImage('first_cover')"
+                                class="btn btn-sm btn-danger position-absolute"
+                                style="bottom: 0; left: 49%">حذف</button>
+                        </div>
+                    @endif
+                </div>
+                <div class="col-6 form-group">
+                    <label class="form-label">تصویر کاور دوم</label>
+                    <input type="file" name="second_cover" onchange="uploadImage(this)">
 
-                @if ($image = imageLoader('second_cover'))
-                    <div class="imageLoader position-relative">
-                        <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
-                        <button type="button" onclick="destroyImage('second_cover')"
-                            class="btn btn-sm btn-danger position-absolute" style="bottom: 0; left: 49%">حذف</button>
-                    </div>
-                @endif
+                    @if ($image = imageLoader('second_cover'))
+                        <div class="imageLoader position-relative">
+                            <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
+                            <button type="button" onclick="destroyImage('second_cover')"
+                                class="btn btn-sm btn-danger position-absolute"
+                                style="bottom: 0; left: 49%">حذف</button>
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 </div>
