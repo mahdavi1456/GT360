7760c90fd73b0a8d83a67a07139fd823b1c6568d
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-14 10:15:46 +0330

    Update setting.blade.php

--- resources/views/front/theme/christmas/setting.blade.php
+++ resources/views/front/theme/christmas/setting.blade.php
@@ -31,18 +31,24 @@
         <div class="row">
             <div class="col form-group">
                 <label class="form-label">عنوان اول</label>
-                <input type="text" name="first_title_sec1" class="form-control" placeholder="عنوان کوچک..."
-                    value="{{ $settingModel->getSetting('first_title_sec1', $account->id) }}">
+                <input type="text" name="title_sec1" class="form-control" placeholder="عنوان کوچک..."
+                    value="{{ $settingModel->getSetting('title_sec1', $account->id) }}">
             </div>
             <div class="col form-group">
-                <label class="form-label">عنوان دوم</label>
-                <input type="text" name="second_title_sec1" class="form-control" placeholder="عنوان بزرگ..."
-                    value="{{ $settingModel->getSetting('second_title_sec1', $account->id) }}">
+                <label class="form-label">زیر عنوان اول</label>
+                <input type="text" name="subtitle_sec1" class="form-control" placeholder="عنوان بزرگ..."
+                    value="{{ $settingModel->getSetting('subtitle_sec1', $account->id) }}">
             </div>
-            <div class="col form-group">
-                <label class="form-label">متن دکمه</label>
-                <input type="text" name="button_title_sec1" class="form-control" placeholder="متن دکمه..."
-                    value="{{ $settingModel->getSetting('button_title_sec1', $account->id) }}">
+            <div class="col-4 form-group ">
+                <label class="form-label ">تصویر اول</label>
+                <input type="file" name="image1_sec1" onchange="uploadImage(this)">
+                @if ($image = imageLoader('image1_sec1'))
+                    <div class="imageLoader position-relative">
+                        <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
+                        <button type="button" onclick="destroyImage('image1_sec1')"
+                            class="btn btn-sm btn-danger position-absolute" style="bottom: 0; left: 49%">حذف</button>
+                    </div>
+                @endif
             </div>
             <div class="col form-group">
                 <label class="form-label">عنوان سوم</label>
