e575188b6b10cf15342c4ae03cde02171ff8abf2
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-14 12:43:07 +0330

    Update setting.blade.php

--- resources/views/front/theme/christmas/setting.blade.php
+++ resources/views/front/theme/christmas/setting.blade.php
@@ -283,11 +283,9 @@
                     <input type="text" name="title_sec3" class="form-control" placeholder="عنوان اول..."
                         value="{{ $settingModel->getSetting('title_sec3', $account->id) }}">
                 </div>
-                <div class="row">
-                    <div class="col-6 form-group">
-                        <label class="form-label">متن اول</label>
-                        <textarea rows="4" name="subtitle_sec3" class="form-control" placeholder="متن اول...">{{ $settingModel->getSetting('first_text', $account->id) }}</textarea>
-                    </div>
+                <div class="col-6 form-group">
+                    <label class="form-label">متن اول</label>
+                    <textarea rows="4" name="subtitle_sec3" class="form-control" placeholder="متن اول...">{{ $settingModel->getSetting('first_text', $account->id) }}</textarea>
                 </div>
             </div>
         </div>
