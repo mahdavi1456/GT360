ac8659b74fd66140fcd42dedbf8b2419ef1b7747
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-14 14:22:58 +0330

    Update setting.blade.php

--- resources/views/front/theme/christmas/setting.blade.php
+++ resources/views/front/theme/christmas/setting.blade.php
@@ -527,6 +527,50 @@
 </div>
 <div class="card card-warning">
     <div class="card-header">
+        <h3 class="card-title pull-right">بخش ششم</h3>
+        <select name="sec6_status" class="form-select pull-left" onchange="this.form.submit()">
+            <option {{ $settingModel->getSetting('sec6_status', $account->id) == 1 ? 'selected' : '' }}
+                value="1">
+                فعال</option>
+            <option {{ $settingModel->getSetting('sec6_status', $account->id) == 0 ? 'selected' : '' }}
+                value="0">
+                غیرفعال</option>
+        </select>
+    </div>
+    @if ($settingModel->getSetting('sec6_status', $account->id) == 1)
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6 form-group">
+                    <label class="form-label">عنوان اول</label>
+                    <input type="text" name="title1_sec6" class="form-control" placeholder="عنوان کوچک..."
+                        value="{{ $settingModel->getSetting('title1_sec4', $account->id) }}">
+                </div>
+                <div class="col form-group">
+                    <label class="form-label">زیر عنوان اول</label>
+                    <input type="text" name="subtitle1_sec6" class="form-control" placeholder="عنوان بزرگ..."
+                        value="{{ $settingModel->getSetting('subtitle1_sec4', $account->id) }}">
+                </div>
+            </div>
+            <div class="col form-group">
+                <label class="form-label">متن دکمه اول </label>
+                <input type="text" name="button1_title_sec6" class="form-control" placeholder="متن دکمه..."
+                    value="{{ $settingModel->getSetting('button1_title_sec6', $account->id) }}">
+            </div>
+            <div class="col form-group">
+                <label class="form-label">متن دکمه دوم</label>
+                <input type="text" name="button2_title_sec6" class="form-control" placeholder="متن دکمه..."
+                    value="{{ $settingModel->getSetting('button2_title_sec6', $account->id) }}">
+            </div>
+            <div class="col form-group">
+                <label class="form-label">متن دکمه سوم</label>
+                <input type="text" name="button3_title_sec6" class="form-control" placeholder="متن دکمه..."
+                    value="{{ $settingModel->getSetting('button3_title_sec6', $account->id) }}">
+            </div>
+        </div>
+    @endif
+</div>
+<div class="card card-warning">
+    <div class="card-header">
         <h3 class="card-title">تماس</h3>
     </div>
     <div class="card-body">
