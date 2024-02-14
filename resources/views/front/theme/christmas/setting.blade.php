63f0bd34ec2845772647d3fcc82ffd1b23b5b3d9
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-14 14:35:51 +0330

    Update setting.blade.php

--- resources/views/front/theme/christmas/setting.blade.php
+++ resources/views/front/theme/christmas/setting.blade.php
@@ -585,6 +585,45 @@
 </div>
 <div class="card card-warning">
     <div class="card-header">
+        <h3 class="card-title pull-right">بخش ششم</h3>
+        <select name="sec7_status" class="form-select pull-left" onchange="this.form.submit()">
+            <option {{ $settingModel->getSetting('sec7_status', $account->id) == 1 ? 'selected' : '' }}
+                value="1">
+                فعال</option>
+            <option {{ $settingModel->getSetting('sec7_status', $account->id) == 0 ? 'selected' : '' }}
+                value="0">
+                غیرفعال</option>
+        </select>
+    </div>
+    @if ($settingModel->getSetting('sec7_status', $account->id) == 1)
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12 form-group ">
+                    <label class="form-label ">تصویر </label>
+                    <input type="file" name="image_sec7" onchange="uploadImage(this)">
+                    @if ($image = imageLoader('image_sec7'))
+                        <div class="imageLoader position-relative">
+                            <img src="{{ asset(ert('tsp') . $image) }}" class="w-100 object-fit-contain">
+                            <button type="button" onclick="destroyImage('image_sec7')"
+                                class="btn btn-sm btn-danger position-absolute"
+                                style="bottom: 0; left: 49%">حذف</button>
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 form-group">
+                    <label class="form-label">عنوان اول</label>
+                    <input type="text" name="title1_sec7" class="form-control" placeholder="عنوان کوچک..."
+                        value="{{ $settingModel->getSetting('title1_sec7', $account->id) }}">
+                </div>
+            </div>
+
+        </div>
+    @endif
+</div>
+<div class="card card-warning">
+    <div class="card-header">
         <h3 class="card-title">تماس</h3>
     </div>
     <div class="card-body">
