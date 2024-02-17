320723094a21c6b6c972d135988d9a94b13c0fe3
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-17 23:04:02 +0330

    Fix

--- resources/views/front/theme/motive/index.blade.php
+++ resources/views/front/theme/motive/index.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="fa">
 	<head>
-		<title>قالب سایت خبری</title>
+		<title>{{ $settingModel->getSetting('title', $accountId, $projectId) }}</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="{{ $settingModel->getSetting('description', $accountId, $projectId) }}">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<script type="application/x-javascript">
 			addEventListener("load", function() {
@@ -12,18 +13,19 @@
 				window.scrollTo(0, 1);
 			}
 		</script>
-		<link href="{{ asset('front-theme-asset/motive/css/bootstrap-3.1.1.min.css') }}" rel="stylesheet" type="text/css">
-		<!-- Custom Theme files -->
+
+        <link rel="stylesheet" type="text/css" href="{{ asset('front-theme-asset/motive/css/bootstrap-3.1.1.min.css') }}">
 		<link href="{{ asset('front-theme-asset/motive/css/owl.carousel.css') }}" rel="stylesheet" type="text/css" media="all" />
-		<link href="{{ asset('front-theme-asset/motive/css/style.css') }}" rel='stylesheet' type='text/css' />
-		<script src="{{ asset('front-theme-asset/motive/js/jquery.min.js') }}"> </script>
+		<link rel="stylesheet" type="text/css" href="{{ asset('front-theme-asset/motive/css/style.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ asset('front-theme-asset/motive/css/flexslider.css') }}" media="screen" />
+        <link rel="stylesheet" href="{{ asset('fonts/' . $settingModel->getSetting('font', $accountId, $projectId) . '/face.css') }}">
+
+        <script src="{{ asset('front-theme-asset/motive/js/jquery.min.js') }}"> </script>
 		<script type="text/javascript" src="{{ asset('front-theme-asset/motive/js/move-top.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('front-theme-asset/motive/js/easing.js') }}"></script>
-		<link rel="stylesheet" href="{{ asset('front-theme-asset/motive/css/flexslider.css') }}" type="text/css" media="screen" />
-		<!--/script-->
 		<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){
+			jQuery(document).ready(function ($) {
+				$(".scroll").click(function (event) {
 					event.preventDefault();
 					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
 				});
