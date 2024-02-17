e36ec4de0566cbd96f7faf6916f29528ab967456
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-17 08:20:51 +0330

    Update chooseTheme.blade.php

--- resources/views/admin/theme/chooseTheme.blade.php
+++ resources/views/admin/theme/chooseTheme.blade.php
@@ -36,7 +36,6 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            @break
                                         @endif
                                     @endforeach
                                 </div>
