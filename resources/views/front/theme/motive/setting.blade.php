69158b07eda434297f5e52fe42f26d7a574fbaf4
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-18 21:20:16 +0330

    Update setting.blade.php

--- resources/views/front/theme/motive/setting.blade.php
+++ resources/views/front/theme/motive/setting.blade.php
@@ -32,26 +32,3 @@
         </div>
     @endif
 </div>
-
-<div class="card card-warning">
-    <div class="card-header">
-        <h3 class="card-title pull-right">گالری</h3>
-        <select name="gallery_status" class="form-select pull-left" onchange="this.form.submit()">
-            <option {{ $settingModel->getSetting('gallery_status', $accountId, $projectId) == 1 ? 'selected' : '' }}
-                    value="1">فعال</option>
-            <option {{ $settingModel->getSetting('gallery_status', $accountId, $projectId) == 0 ? 'selected' : '' }}
-                    value="0">غیرفعال</option>
-        </select>
-    </div>
-    @if ($settingModel->getSetting('gallery_status', $accountId, $projectId) == 1)
-        <div class="card-body">
-            <div class="row">
-                <div class="col-6 form-group">
-                    <label class="form-label">عنوان گالری</label>
-                    <input name="gallery_title" class="form-control" placeholder="عنوان گالری..."
-                        {{ $settingModel->getSetting('event_title', $accountId, $projectId) }}>
-                </div>
-            </div>
-        </div>
-    @endif
-</div>
