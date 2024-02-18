7dc1e7fe3f3b7d4faf40542a2058b2c342867eb4
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-02-18 22:48:09 +0330

    Update setting.blade.php

--- resources/views/front/theme/motive/setting.blade.php
+++ resources/views/front/theme/motive/setting.blade.php
@@ -14,7 +14,7 @@
                 <div class="col-4 form-group">
                     <label class="form-label">عنوان بخش اول</label>
                     <input name="section1_title" class="form-control" placeholder="عنوان بخش اول..."
-                        {{ $settingModel->getSetting('section1_title', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section1_title', $accountId, $projectId) }}">
                 </div>
                 <div class="col-4 form-group">
                     <label class="form-label">تصویر بخش اول</label>
@@ -31,7 +31,7 @@
                 <div class="col-4 form-group">
                     <label class="form-label">لیست محتوا بخش اول</label>
                     <input name="section1_component" class="form-control" placeholder="لیست محتوا بخش اول..."
-                        {{ $settingModel->getSetting('section1_component', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section1_component', $accountId, $projectId) }}">
                 </div>
             </div>
         </div>
@@ -54,12 +54,12 @@
                 <div class="col-6 form-group">
                     <label class="form-label">عنوان بخش دوم</label>
                     <input name="section2_title" class="form-control" placeholder="عنوان بخش دوم..."
-                        {{ $settingModel->getSetting('section2_title', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section2_title', $accountId, $projectId) }}">
                 </div>
                 <div class="col-6 form-group">
                     <label class="form-label">لیست محتوا بخش دوم</label>
                     <input name="section2_component" class="form-control" placeholder="لیست محتوا بخش دوم..."
-                        {{ $settingModel->getSetting('section2_component', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section2_component', $accountId, $projectId) }}">
                 </div>
             </div>
         </div>
@@ -82,12 +82,12 @@
                 <div class="col-6 form-group">
                     <label class="form-label">عنوان بخش سوم</label>
                     <input name="section3_title" class="form-control" placeholder="عنوان بخش سوم..."
-                        {{ $settingModel->getSetting('section3_title', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section3_title', $accountId, $projectId) }}">
                 </div>
                 <div class="col-6 form-group">
                     <label class="form-label">لیست محتوا بخش سوم</label>
                     <input name="section3_component" class="form-control" placeholder="لیست محتوا بخش سوم..."
-                        {{ $settingModel->getSetting('section3_component', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section3_component', $accountId, $projectId) }}">
                 </div>
             </div>
         </div>
@@ -110,12 +110,12 @@
                 <div class="col-6 form-group">
                     <label class="form-label">عنوان بخش چهارم</label>
                     <input name="section4_title" class="form-control" placeholder="عنوان بخش چهارم..."
-                        {{ $settingModel->getSetting('section4_title', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section4_title', $accountId, $projectId) }}">
                 </div>
                 <div class="col-6 form-group">
                     <label class="form-label">لیست محتوا بخش چهارم</label>
                     <input name="section4_component" class="form-control" placeholder="لیست محتوا بخش چهارم..."
-                        {{ $settingModel->getSetting('section4_component', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section4_component', $accountId, $projectId) }}">
                 </div>
             </div>
         </div>
@@ -138,12 +138,12 @@
                 <div class="col-6 form-group">
                     <label class="form-label">عنوان بخش پنجم</label>
                     <input name="section5_title" class="form-control" placeholder="عنوان بخش پنجم..."
-                        {{ $settingModel->getSetting('section5_title', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section5_title', $accountId, $projectId) }}">
                 </div>
                 <div class="col-6 form-group">
                     <label class="form-label">لیست محتوا بخش پنجم</label>
                     <input name="section5_component" class="form-control" placeholder="لیست محتوا بخش پنجم..."
-                        {{ $settingModel->getSetting('section5_component', $accountId, $projectId) }}>
+                        value="{{ $settingModel->getSetting('section5_component', $accountId, $projectId) }}">
                 </div>
             </div>
         </div>
