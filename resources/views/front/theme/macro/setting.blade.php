5a3352fde3498d1333ce29893d3f148ca92fa5e0
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-27 12:01:53 +0330

    Update setting.blade.php

--- resources/views/front/theme/macro/setting.blade.php
+++ resources/views/front/theme/macro/setting.blade.php
@@ -642,6 +642,7 @@
                         value="{{ $settingModel->getSetting('subtitle3_sec8', $account->id, $projectId) }}">
                 </div>
             </div>
+            <hr>
             <div class="row">
                 <div class="col-4 form-group ">
                     <label class="form-label ">تصویر چهارم</label>
