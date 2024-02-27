c14231e7ed82f1c0beb4dd7b977e97088cbde2f6
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-02-27 13:21:13 +0330

    Update setting.blade.php

--- resources/views/front/theme/macro/setting.blade.php
+++ resources/views/front/theme/macro/setting.blade.php
@@ -931,7 +931,7 @@
 </div>
 <div class="card card-warning">
     <div class="card-header">
-        <h3 class="card-title pull-right">بخش یازدهم</h3>
+        <h3 class="card-title pull-right">بخش دوازدهم</h3>
         <select name="sec12_status" class="form-select pull-left" onchange="this.form.submit()">
             <option {{ $settingModel->getSetting('sec12_status', $account->id, $projectId) == 1 ? 'selected' : '' }}
                 value="1">
@@ -953,3 +953,28 @@
         </div>
     @endif
 </div>
+<div class="card card-warning">
+    <div class="card-header">
+        <h3 class="card-title pull-right">بخش سیزدهم</h3>
+        <select name="sec13_status" class="form-select pull-left" onchange="this.form.submit()">
+            <option {{ $settingModel->getSetting('sec13_status', $account->id, $projectId) == 1 ? 'selected' : '' }}
+                value="1">
+                فعال</option>
+            <option {{ $settingModel->getSetting('sec13_status', $account->id, $projectId) == 0 ? 'selected' : '' }}
+                value="0">
+                غیرفعال</option>
+        </select>
+    </div>
+    @if ($settingModel->getSetting('sec13_status', $account->id, $projectId) == 1)
+        <div class="card-body">
+            <div class="row">
+                <div class="col form-group">
+                    <label class="form-label">عنوان اول</label>
+                    <input type="text" name="title1_sec13" class="form-control" placeholder="عنوان اول..."
+                        value="{{ $settingModel->getSetting('title1_sec13', $account->id, $projectId) }}">
+                </div>
+            </div>
+        </div>
+    @endif
+</div>
+
