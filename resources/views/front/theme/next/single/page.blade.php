dd1af7d64a39b112e7f951465991ea8ea2959329
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-03-08 23:12:58 +0330

    Update page.blade.php

--- resources/views/front/theme/next/single/page.blade.php
+++ resources/views/front/theme/next/single/page.blade.php
@@ -1,5 +1,5 @@
 @include('front.theme.next.parts.header')
-
+@php $page = $siteEngine->getPage($slug); @endphp
 <!-- inner page banner -->
 <div id="inner_banner" class="section inner_banner_section">
     <div class="container">
@@ -29,8 +29,8 @@
             <div class="col-md-12">
                 <div class="full">
                     <div class="main_heading text_align_center">
-                        <h2>ما شرکت پیشرو هستیم</h2>
-                        <p class="large">سریعترین خدمات تعمیر با بهترین قیمت!</p>
+                        <h2>{{ $page->title }}</h2>
+                        <p class="large">{{ $page->abstract }}</p>
                     </div>
                 </div>
             </div>
@@ -48,72 +48,14 @@
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12 about_feature_img padding_right_0">
-                <div class="full text_align_center"> <img class="img-responsive" src="images/it_service/post-06.jpg" alt="#" /> </div>
-            </div>
-        </div>
-        <div class="row" style="margin-top: 35px">
-            <div class="col-md-8">
-                <div class="full margin_bottom_30">
-                    <div class="accordion border_circle">
-                        <div class="bs-example">
-                            <div class="panel-group" id="accordion">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <p class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><i class="fa fa-bar-chart" aria-hidden="true"></i> بازیابی کامل از درایو محلی و خارجی<i class="fa fa-angle-down"></i></a> </p>
-                                    </div>
-                                    <div id="collapseOne" class="panel-collapse collapse in">
-                                        <div class="panel-body">
-                                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <p class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><i class="fa fa-plane"></i> بازیابی عکس ، تصویر ، ویدئو و صدا<i class="fa fa-angle-down"></i></a> </p>
-                                    </div>
-                                    <div id="collapseTwo" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <p class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><i class="fa fa-star"></i> بازیابی تلفن همراه<i class="fa fa-angle-down"></i></a> </p>
-                                    </div>
-                                    <div id="collapseThree" class="panel-collapse collapse">
-                                        <div class="panel-body">
-                                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <p class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour"><i class="fa fa-bar-chart" aria-hidden="true"></i> بازیابی کامل از درایو محلی و خارجی<i class="fa fa-angle-down"></i></a> </p>
-                                    </div>
-                                    <div id="collapseFour" class="panel-collapse collapse in">
-                                        <div class="panel-body">
-                                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="full" style="margin-top: 35px;">
-                    <h3>به بازیابی فایل نیاز دارید؟</h3>
-                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است ..</p>
-                    <p><a class="btn main_bt" href="#">بیشتر بخوانید</a></p>
+                <div class="full text_align_center">
+                    <img class="img-responsive" src="{{ asset('front-theme-asset/next/images/it_service/post-06.jpg') }}" alt="#" />
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- end section -->
-
 
 @include('front.theme.next.parts.brands')
 
