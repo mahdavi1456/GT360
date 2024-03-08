afa8497db28ad51b4c79e6d148d237c162c3edb2
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-03-08 15:01:41 +0330

    Update footer.blade.php

--- resources/views/front/theme/next/parts/footer.blade.php
+++ resources/views/front/theme/next/parts/footer.blade.php
@@ -79,26 +79,26 @@
 </footer>
 <!-- end footer -->
 <!-- js section -->
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="{{ asset('front-theme-asset/next/') }} js/jquery.min.js"></script>
+<script src="{{ asset('front-theme-asset/next/') }} js/bootstrap.min.js"></script>
 <!-- menu js -->
-<script src="js/menumaker.js"></script>
+<script src="{{ asset('front-theme-asset/next/') }} js/menumaker.js"></script>
 <!-- wow animation -->
-<script src="js/wow.js"></script>
+<script src="{{ asset('front-theme-asset/next/') }}js/wow.js"></script>
 <!-- custom js -->
-<script src="js/custom.js"></script>
+<script src="{{ asset('front-theme-asset/next/') }}js/custom.js"></script>
 <!-- revolution js files -->
-<script src="revolution/js/jquery.themepunch.tools.min.js"></script>
-<script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-<script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/jquery.themepunch.tools.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.actions.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.migration.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+<script src="{{ asset('front-theme-asset/next/revolution/js/extensions/revolution.extension.video.min.js') }}"></script>
 <!-- map js -->
 <script>
     // This example adds a marker to indicate the position of Bondi Beach in Sydney,
