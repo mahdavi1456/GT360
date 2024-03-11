01e29841b494306ab17373ac1c7cf10018b9c1f7
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-03-11 12:43:43 +0330

    fix

--- resources/views/front/theme/roma/index.blade.php
+++ resources/views/front/theme/roma/index.blade.php
@@ -187,7 +187,7 @@
                 </div>
                 <div class="col-md-12 col-sm-12"></div>
                 @if ($siteEngine->getPosts('product')->count() > 0)
-                    @foreach ($siteEngine->getposta('product') as $product)
+                    @foreach ($siteEngine->getPosts('product') as $product)
                         <div class="col-md-3 col-sm-3 wow fadeIn" data-wow-delay="0.6s">
                             <a href="{{ $prouduct->getSingleUrl() }}" data-lightbox-gallery="portfolio-gallery">
                                 <img src="{{$prouduct->getImageUrl()}}" alt="">
