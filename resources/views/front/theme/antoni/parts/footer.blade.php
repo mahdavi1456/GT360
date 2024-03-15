315a01e1e90e4d2c146e16f2bad3e25c64df9e60
S.Morteza Mahdavi <mahdavy1456@yahoo.com>
2024-03-15 13:59:02 +0330

    Add Antoni Shop Theme

---
+++ resources/views/front/theme/antoni/parts/footer.blade.php
@@ -0,0 +1,318 @@
+
+<!--footer area start-->
+<footer class="footer_widgets">
+    <!--newsletter area start-->
+    <div class="newsletter_area">
+        <div class="container">
+            <div class="newsletter_inner">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-5">
+                        <div class="newsletter_sing_up">
+                            <h3>عضویت در خبرنامه</h3>
+                            <p>با عضویت از <span>30% تخفیف</span> بهره مند شوید</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-7">
+                        <div class="subscribe_content">
+                            <p><strong>به 226,000+ مشترک ما</strong> بپیوندید و از تخفیف های ویژه هفتگی مخصوص مشترکین خبرنامه بهره مند شوید.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-12">
+                        <div class="subscribe_form">
+                            <form id="mc-form" class="mc-form footer-newsletter">
+                                <input id="mc-email" type="email" autocomplete="off" placeholder="... آدرس ایمیل شما" dir="ltr">
+                                <button id="mc-submit">اشتراک</button>
+                            </form>
+                            <!-- mailchimp-alerts Start -->
+                            <div class="mailchimp-alerts text-centre">
+                                <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+                                <div class="mailchimp-success"></div><!-- mailchimp-success end -->
+                                <div class="mailchimp-error"></div><!-- mailchimp-error end -->
+                            </div><!-- mailchimp-alerts end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--newsletter area end-->
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-5 col-sm-7">
+                    <div class="widgets_container contact_us">
+                        <h3>دریافت برنامه</h3>
+                        <div class="aff_content">
+                            <p>برنامه <strong>آنتومی</strong> هم اکنون در گوگل پلی و اپل استور آماده دریافت است.</p>
+                        </div>
+                        <div class="app_img">
+                            <figure class="app_img">
+                                <a href="#"><img src="assets/img/icon/icon-appstore.png" alt=""></a>
+                            </figure>
+                            <figure class="app_img">
+                                <a href="#"><img src="assets/img/icon/icon-googleplay.png" alt=""></a>
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-5">
+                    <div class="widgets_container widget_menu">
+                        <h3>اطلاعات</h3>
+                        <div class="footer_menu">
+                            <ul>
+                                <li><a href="about.html">درباره ما</a></li>
+                                <li><a href="#">نحوه ارسال</a></li>
+                                <li><a href="#">محصولات جدید</a></li>
+                                <li><a href="#">محصولات برتر</a></li>
+                                <li><a href="my-account.html">حساب کاربری</a></li>
+                                <li><a href="#">سابقه خرید</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6">
+                    <div class="widgets_container widget_menu">
+                        <h3>حساب کاربری</h3>
+                        <div class="footer_menu">
+                            <ul>
+                                <li><a href="my-account.html">حساب کاربری</a></li>
+                                <li><a href="cart.html">سبد خرید</a></li>
+                                <li><a href="wishlist.html">علاقه‌مندی‌ها</a></li>
+                                <li><a href="#">کاهش قیمت‌ها</a></li>
+                                <li><a href="#">سابقه خرید</a></li>
+                                <li><a href="#">سفارشات بین المللی</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-5 col-sm-6">
+                    <div class="widgets_container widget_menu">
+                        <h3>خدمات مشتری</h3>
+                        <div class="footer_menu">
+                            <ul>
+                                <li><a href="#">نقشه سایت</a></li>
+                                <li><a href="my-account.html">حساب کاربری</a></li>
+                                <li><a href="#">نحوه ارسال</a></li>
+                                <li><a href="#">سابقه خرید</a></li>
+                                <li><a href="wishlist.html">علاقه‌مندی‌ها</a></li>
+                                <li><a href="#">ویژه ها</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-7 col-sm-12">
+                    <div class="widgets_container">
+                        <h3>اطلاعات تماس</h3>
+                        <div class="footer_contact">
+                            <div class="footer_contact_inner">
+                                <div class="contact_icone">
+                                    <img src="assets/img/icon/icon-phone.png" alt="">
+                                </div>
+                                <div class="contact_text">
+                                    <p>تلفن تماس 24 ساعته: <br> <strong class="ltr-text">(+98) 123 456 789</strong></p>
+                                </div>
+                            </div>
+                            <p>تبریز، چهار راه آبرسان، فلکه دانشگاه، برج تجاری بلور، طبقه 456، واحد 45</p>
+                        </div>
+
+                        <div class="footer_social">
+                            <ul>
+                                <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a class="instagram" href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a class="rss" href="#"><i class="fa fa-rss"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="copyright_area">
+                        <p>ارائه شده در وب‌سایت <a href="https://market.radib.com/store/hesdesign/" target="_blank">=> فایل مارکت رادیب <= </a></p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="footer_payment text-right">
+                        <img src="assets/img/icon/payment.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--footer area end-->
+
+<!-- modal area start-->
+<div class="modal fade" id="modal_box" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span>
+            </button>
+            <div class="modal_body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5 col-md-5 col-sm-12">
+                            <div class="modal_tab">
+                                <div class="tab-content product-details-large">
+                                    <div class="tab-pane fade show active" id="tab1" role="tabpanel">
+                                        <div class="modal_tab_img">
+                                            <a href="#"><img src="assets/img/product/productbig2.jpg" alt=""></a>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="tab2" role="tabpanel">
+                                        <div class="modal_tab_img">
+                                            <a href="#"><img src="assets/img/product/productbig3.jpg" alt=""></a>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="tab3" role="tabpanel">
+                                        <div class="modal_tab_img">
+                                            <a href="#"><img src="assets/img/product/productbig4.jpg" alt=""></a>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="tab4" role="tabpanel">
+                                        <div class="modal_tab_img">
+                                            <a href="#"><img src="assets/img/product/productbig5.jpg" alt=""></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal_tab_button">
+                                    <ul class="nav product_navactive owl-carousel" role="tablist">
+                                        <li>
+                                            <a class="nav-link active" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="false"><img src="assets/img/product/product1.jpg" alt=""></a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false"><img src="assets/img/product/product6.jpg" alt=""></a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link button_three" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false"><img src="assets/img/product/product9.jpg" alt=""></a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link" data-toggle="tab" href="#tab4" role="tab" aria-controls="tab4" aria-selected="false"><img src="assets/img/product/product14.jpg" alt=""></a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-7 col-sm-12">
+                            <div class="modal_right">
+                                <div class="modal_title mb-10">
+                                    <h2>گوشی هوشمند سامسونگ A50</h2>
+                                </div>
+                                <div class="modal_price mb-10">
+                                    <span class="new_price">64,000 تومان</span>
+                                    <span class="old_price">78,000 تومان</span>
+                                </div>
+                                <div class="modal_description mb-15">
+                                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای </p>
+                                </div>
+                                <div class="variants_selects">
+                                    <div class="variants_size">
+                                        <h2>اندازه</h2>
+                                        <select class="select_option">
+                                            <option selected value="1">کوچک</option>
+                                            <option value="1">متوسط</option>
+                                            <option value="1">بزرگ</option>
+                                            <option value="1">XL</option>
+                                            <option value="1">XXL</option>
+                                        </select>
+                                    </div>
+                                    <div class="variants_color">
+                                        <h2>رنگ</h2>
+                                        <select class="select_option">
+                                            <option selected value="1">بنفش</option>
+                                            <option value="1">اطلسی</option>
+                                            <option value="1">مشکی</option>
+                                            <option value="1">صورتی</option>
+                                            <option value="1">نارنجی</option>
+                                        </select>
+                                    </div>
+                                    <div class="modal_add_to_cart">
+                                        <form action="#">
+                                            <input min="1" max="100" step="2" value="1" type="number">
+                                            <button type="submit">افزودن به سبد</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="modal_social">
+                                    <h2>اشتراک گذاری این محصول</h2>
+                                    <ul>
+                                        <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li class="pinterest"><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                        <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                        <li class="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- modal area end-->
+
+<!--news letter popup start (uncomment lines 661-686 in main.js to show this)-->
+<div class="newletter-popup">
+    <div id="boxes" class="newletter-container">
+        <div id="dialog" class="window">
+            <div id="popup2">
+                <span class="b-close"><span>بستن</span></span>
+            </div>
+            <div class="box">
+                <div class="newletter-title">
+                    <h2>خبرنامه</h2>
+                </div>
+                <div class="box-content newleter-content">
+                    <label class="newletter-label">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</label>
+                    <div id="frm_subscribe">
+                        <form name="subscribe" id="subscribe_popup">
+                            <input type="text" value="" name="subscribe_pemail" id="subscribe_pemail" placeholder="... ایمیل خود را وارد کنید" dir="ltr">
+                            <input type="hidden" value="" name="subscribe_pname" id="subscribe_pname">
+                            <div id="notification"></div>
+                            <a class="theme-btn-outlined" onclick="email_subscribepopup()"><span>اشتراک</span></a>
+                        </form>
+                        <div class="subscribe-bottom">
+                            <input type="checkbox" id="newsletter_popup_dont_show_again">
+                            <label for="newsletter_popup_dont_show_again">دیگر این پاپ آپ را نمایش نده</label>
+                        </div>
+                    </div>
+                    <!-- /#frm_subscribe -->
+                </div>
+                <!-- /.box-content -->
+            </div>
+        </div>
+
+    </div>
+    <!-- /.box -->
+</div>
+<!--news letter popup start-->
+
+
+
+
+<!-- JS
+============================================ -->
+
+<!-- Plugins JS -->
+<script src="{{ asset('front-theme-asset/js/plugins.js') }}"></script>
+
+<!-- Main JS -->
+<script src="{{ asset('front-theme-asset/js/main.js') }}"></script>
+
+
+
+</body>
+
+
+<!-- => Designed by HesDesign @ https://market.radib.com <= antomi/antomi/index.html => از اینکه با خرید رسمی قالب در فایل مارکت رادیب، از ما حمایت می کنید سپاسگزاریم <=  09:13:54 GMT -->
+</html>
