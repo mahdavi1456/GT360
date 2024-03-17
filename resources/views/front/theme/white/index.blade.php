acbb42b3d9819b8630f485677141bb7b59a00706
faezeh1370 <76598207+faezeh1370@users.noreply.github.com>
2024-03-17 13:44:10 +0330

    Update White Theme

--- resources/views/front/theme/white/index.blade.php
+++ resources/views/front/theme/white/index.blade.php
@@ -2,21 +2,21 @@
 <!DOCTYPE html>
 <html lnag="fa">
 <head>
-    <title>{{ $settingModel->getSetting('title', $accountId, $projectId) }}</title>
+    <title>{{ $siteEngine->getSetting('title') }}</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="{{ $settingModel->getSetting('title', $accountId, $projectId) }}">
+    <meta name="description" content="{{ $siteEngine->getSetting('title') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('front-theme-asset/white/css/style.css') }}">
     <style type="text/css">
         body {
-            background: {{ $settingModel->getSetting('body_bg_color', $accountId, $projectId) }};
+            background: {{ $siteEngine->getSetting('body_bg_color') }};
         }
     </style>
 </head>
 <body>
 <div class="container">
     <div class="LogoBox">
-        <img id="logo" src="{{ asset(ert('tsp') . $settingModel->getSetting('background_cover', $accountId, $projectId)) }}" width="450px"/>
+        <img id="logo" src="{{ asset(ert('tsp') . $siteEngine->getSetting('background_cover')) }}" width="450px"/>
     </div>
 </div>
 </body>
